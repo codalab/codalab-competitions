05c718122fb0dd04b2bc1a23465c21a084963506
Beau Hargis <beau@subobscur.us>
2013-07-01 16:23:58 -0700

    Made sqlite3 default db for development.

--- codalab/codalab/settings/__init__.py
+++ codalab/codalab/settings/__init__.py
@@ -19,8 +19,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'dev_db.sqlite',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
