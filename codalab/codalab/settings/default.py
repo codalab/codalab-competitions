a777b65548df854bca168904a98967adedb211aa
Beau Hargis <beau@subobscur.us>
2013-07-06 17:06:40 -0700

    verified requirements and tweaked some settings

--- codalab/codalab/settings/default.py
+++ codalab/codalab/settings/default.py
@@ -1,6 +1,13 @@
 from .base import Base
+import os
 
 class Dev(Base):
-   pass
+   HAYSTACK_CONNECTIONS = {
+      'default': {
+          'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+          'PATH': os.path.join(Base.PROJECT_DIR, 'whoosh_index'),
+      },
+   }
+
 
 
