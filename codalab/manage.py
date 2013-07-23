b69d5ea13e6a57ee9ccbe67f83af68d5ada2cc92
Quebecisnice <carden.justin@gmail.com>
2013-07-23 11:23:34 -0700

    fixed settings and manage errors

--- codalab/manage.py
+++ codalab/manage.py
@@ -2,9 +2,10 @@
 import os
 import sys
 
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codalab.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
     os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
 
-    from configurations.management import execute_from_command_line
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
