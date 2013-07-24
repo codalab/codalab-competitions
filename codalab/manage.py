c1de208d600148ea36d67575e9ecd23b10b4dc5a
Quebecisnice <carden.justin@gmail.com>
2013-07-24 09:57:23 -0700

    Updating for pull request

--- codalab/manage.py
+++ codalab/manage.py
@@ -4,8 +4,15 @@ import sys
 
 
 if __name__ == "__main__":
+<<<<<<< HEAD
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+=======
+    if os.environ.get('LOCAL_CONFIGURATION_PATH'):
+        sys.path.append(os.environ.get('LOCAL_CONFIGURATION_PATH'))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codalab.settings")
+>>>>>>> master
     os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
+    os.environ.setdefault('DJANGO_LOCAL_CONFIGURATION', 'codalab.settings.local_configuration')
 
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
