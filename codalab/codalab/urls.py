38dc009ab5ac6d003d52897a97ebde834ca10500
Ivan R. Judson <irjudson@gmail.com>
2013-07-26 09:55:43 -0700

    Rework of front end done.
    I have not fully replaced everything. Just the functionality we need
    first. I'll have to march through the rest of it as we go.

--- codalab/codalab/urls.py
+++ codalab/codalab/urls.py
@@ -8,33 +8,21 @@ from django.views.generic import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^starter/', TemplateView.as_view(template_name="foundation/starter.html")),
     url(r'', include('apps.web.urls')),
-    (r'^accounts/', include('allauth.urls')),
-    (r'^search/', include('haystack.urls')),
-    (r'^api/', include('apps.api.routers')),
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'^search/', include('haystack.urls')),
+    url(r'^api/', include('apps.api.routers')),
     
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # Static files
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT}),
+
+    # JS Reverse for saner AJAX calls
+    url(r'^jsreverse/$', 'django_js_reverse.views.urls_js', name='js_reverse')
 ) 
-
-
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += patterns(
-    '',
-    url(r'^jsreverse/$', 'django_js_reverse.views.urls_js', name='js_reverse'),
-)
-
-
-
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += patterns(
-    '',
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT,
-            }),
-    )
