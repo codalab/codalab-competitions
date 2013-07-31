7ae4ebd3613b0fee72356aa245ffb9ae2cfc4d95
Ivan R. Judson <irjudson@gmail.com>
2013-07-31 06:03:45 -0700

    Added django analytical configuration
    This enables analytics tracking of CodaLab. Google Analytics initial
    service configured.

--- codalab/codalab/settings/base.py
+++ codalab/codalab/settings/base.py
@@ -178,9 +178,9 @@ class Base(Settings):
 
     # Search app
     'haystack',
+    
     # Migration app
     #'south',
-
 
     # CodaLab apps
     'apps.authenz',
@@ -196,15 +196,18 @@ class Base(Settings):
    OPTIONAL_APPS = [ 'django_wsgiserver',]
    INTERNAL_IPS = []
 
+   # Authentication configuration
    LOGIN_REDIRECT_URL = '/my'
-
    ANONYMOUS_USER_ID = -1
-
    ACCOUNT_AUTHENTICATION_METHOD='username_email'
    ACCOUNT_EMAIL_REQUIRED=True
    ACCOUNT_USERNAME_REQUIRED=False
    ACCOUNT_EMAIL_VERIFICATION='none'
 
+   # Django Analytical configuration
+   GOOGLE_ANALYTICS_PROPERTY_ID = 'UA-42847758-1'
+
+   # Compress Configuration
    COMPRESS_PRECOMPILERS = (
     ('text/less', 'lessc {infile} {outfile}'),
     ('text/typescript', 'tsc {infile} --out {outfile}'),
