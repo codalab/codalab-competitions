6b1f9cbbe1c469cd55a43ac0829375ba9111efda
Beau Hargis <beau@subobscur.us>
2013-08-07 14:38:18 -0700

    Added default config for celerysrv

--- codalab/compsrv/celerysrv.py
+++ codalab/compsrv/celerysrv.py
@@ -2,7 +2,11 @@ from __future__ import absolute_import
 from celery import Celery
 
 celery = Celery('tasks' )
-celery.config_from_envvar('CELERY_CONFIG')
-
+try:
+    celery.config_from_envvar('CELERY_CONFIG')
+except AttributeError:
+    celery.conf.update( BROKER_URL='memory',
+                        CELERY_IMPORTS = ('apps.web.tasks',)
+                        )
 if __name__ == '__main__':
     celery.start()
