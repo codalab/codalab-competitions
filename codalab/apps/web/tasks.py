c67f466c089221820c0101a62a1b3116b4351f50
Quebecisnice <carden.justin@gmail.com>
2013-08-12 21:23:31 -0700

    Added Celery worker queue, RabbitMQ as broker and tasks to build repositories, input, output, YAML

--- codalab/apps/web/tasks.py
+++ codalab/apps/web/tasks.py
@@ -6,15 +6,18 @@ from django.dispatch import receiver
 #from compsrv.celerysrv import celery
 import celery
 from celery.signals import task_success, task_failure, task_revoked, task_sent
+from codalab.settings import base
 
 import models
+
+main = base.Base.SITE_ROOT
+
 
 @celery.task(name='competition.create_submission_run')
 def submission_run(url,submission_id):
     submission = models.CompetitionSubmission.objects.get(pk=submission_id)
     
     
-
 @celery.task(name='competition.validate_submission')
 def validate_submission(url,submission_id):
     """
@@ -33,3 +36,32 @@ def task_success_handler(sender, result=None, **kwargs):
     s = models.CompetitionSubmission.objects.get(pk=result)
     s.set_status('rejected', force_save=True)
    
+
+
+# Bundle Tasks
+@celery.task
+def create_directory(bundleid):
+    import subprocess
+    bundle = models.Bundle.objects.get(id=bundleid)
+    args = ['cd repositories && mkdir -p '+ bundle.name]
+    subprocess.check_output(args, shell=True)
+    bundle.path = main+'/repositories/'+bundle.name
+    bundle.save()
+    print bundle.path
+    sub_directories.delay(bundleid)
+
+
+@celery.task
+def sub_directories(bundleid):
+    import subprocess
+    bundle = models.Bundle.objects.get(id=bundleid)
+    args = ['cd repositories/'+bundle.name+' && mkdir -p input && mkdir -p output']
+    subprocess.check_output(args, shell=True)
+    bundle.inputpath = bundle.path+'/input'
+    bundle.outputpath = bundle.path+'/output'
+    bundle.save()
+    print "The directories have been created"
+    args = ['cd '+bundle.path+' && touch bundle.yaml']
+    subprocess.check_output(args, shell=True)
+    bundle.save()
+    print "The bundle yaml has been created"
--- codalab/apps/web/urls/base.py
+++ codalab/apps/web/urls/base.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
                        url(r'^my/', include('apps.web.urls.my')),
                        url(r'^competitions/', include('apps.web.urls.competitions')),                       
                        url(r'^experiments/', include('apps.web.urls.experiments')),
-                       url(r'^help/', include('apps.web.urls.help'))
+                       url(r'^help/', include('apps.web.urls.help')),
+                       url(r'^bundles/', include('apps.web.urls.bundles')),
+                       url(r'^runs/', include('apps.web.urls.runs'))
                        )
