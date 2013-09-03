37076acea435434d76852856cfc8fffb8e9ec049
Christophe Poulain <cpoulain@live.com>
2013-09-03 11:58:34 -0700

    Improve updates of submission tables

--- codalab/apps/web/static/site/js/Competition.js
+++ codalab/apps/web/static/site/js/Competition.js
@@ -1,8 +1,7 @@
-
 var Competition;
 (function (Competition) {
 
-    Competition.invokePhaseButtonOnOpen = function(id) {
+    Competition.invokePhaseButtonOnOpen = function (id) {
         var btn = $("#" + id + " .button.selected")[0];
         if (btn === undefined) {
             btn = $("#" + id + " .button.active")[0];
@@ -13,7 +12,7 @@ var Competition;
         btn.click();
     }
 
-    Competition.loadTabContent = function() {
+    Competition.loadTabContent = function () {
         var name = $.trim(window.location.hash).toLowerCase();
         if (name == "#results") { $('#Results').click(); }
         if (name == "#participate-submit_results") { $('#Participate').click(); }
@@ -62,7 +61,7 @@ var Competition;
         });
     };
 
-    Competition.getPhaseSubmissions = function(competitionId, phaseId, cstoken) {
+    Competition.getPhaseSubmissions = function (competitionId, phaseId, cstoken) {
         $(".competition_submissions").html("").append("<div class='competitionPreloader'></div>").children().css({ 'top': '200px', 'display': 'block' });
         var url = "/competitions/" + competitionId + "/submissions/" + phaseId;
         $.ajax({
@@ -99,20 +98,19 @@ var Competition;
                         }
                     },
                     success: function (response) {
-                        //var subNumber = response.submission_number.toString();
-                        //var subSubmitted = response.submitted_at;
-                        //var subStatus = "Unknonw";
-                        //switch (response.status) {
-                        //    case 1: subStatus = "Submitted"; break;
-                        //    case 6: subStatus = "Running"; break;
-                        //    case 7: subStatus = "Failed"; break;
-                        //    case 8: subStatus = "Cancelled"; break;
-                        //    case 9: subStatus = "Finished"; break;
-                        //}
-                        //var x = $("#user_results").dataTable().fnAddData([subNumber, subSubmitted, subStatus, "", "", ""]);
-                        //$('#fileUploadButton').text("Submit Results");
-                        //$('#fileUploadButton').removeClass('disabled');
-                        window.location.reload();
+                        $('#user_results .enclosed-foundicon-minus').click(); // workaround until table update is able to preserve "open/close"-state of a row
+                        prTable.fnDestroy();
+                        $("#user_results").append(Competition.displayNewSubmission(response));
+                        $('#user_results #' + response.id + ' .enclosed-foundicon-plus').on("click", function () { Competition.showOrHideSubmissionDetails(this) });
+                        prTable = $("#user_results").dataTable({
+                            'bPaginate': false,
+                            'bInfo': false,
+                            'bFilter': false,
+                            'bAutoWidth': false,
+                            'bSort': false
+                        });
+                        $('#fileUploadButton').removeClass("disabled");
+                        $('#fileUploadButton').text("Submit Results");
                     },
                     fail: function (jqXHR) {
                         $('#details').html("An error occurred (" + jqXHR.status + " - " + jqXHR.responseJSON.detail + ")");
@@ -121,7 +119,7 @@ var Competition;
                     }
                 });
 
-                var prTable = $("#user_results").dataTable({
+                prTable = $("#user_results").dataTable({
                     'bPaginate': false,
                     'bInfo': false,
                     'bFilter': false,
@@ -148,39 +146,7 @@ var Competition;
                 });
                 */
                 $('#user_results .enclosed-foundicon-plus').on('click', function () {
-                    var nTr = $(this).parents('tr')[0];
-                    if (prTable.fnIsOpen(nTr)) {
-                        $(this).removeClass("enclosed-foundicon-minus");
-                        $(this).addClass("enclosed-foundicon-plus");
-                        prTable.fnClose(nTr);
-                    }
-                    else {
-                        $(this).removeClass("enclosed-foundicon-plus");
-                        $(this).addClass("enclosed-foundicon-minus");
-                        var elem = $("#submission_details_template .submission_details").clone();
-                        elem.find("a").each(function (i) { $(this).attr("href", $(this).attr("href").replace("_", nTr.id)) });
-                        var state = $(nTr).find("input[name='state']").val();
-                        if (state == 1) {
-                            var btn = elem.find("button");
-                            btn.removeClass("hide");
-                            var submitted = $(nTr).find(".status").hasClass("submitted");
-                            var competition = $("#competitionId").val();
-                            if (submitted) {
-                                btn.addClass("leaderBoardRemove");
-                                btn.text("Remove from Leaderboard");
-                                btn.on('click', function () {
-                                    removeFromLeaderboard(competition, nTr.id, cstoken);
-                                });
-                            } else {
-                                btn.addClass("leaderBoardSubmit");
-                                btn.text("Submit to Leaderboard");
-                                btn.on('click', function () {
-                                    addToLeaderboard(competition, nTr.id, cstoken);
-                                });
-                            }
-                        }
-                        prTable.fnOpen(nTr, elem, 'details');
-                    }
+                    Competition.showOrHideSubmissionDetails(this);
                 });
             },
             error: function (xhr, status, err) {
@@ -189,7 +155,7 @@ var Competition;
         });
     }
 
-    Competition.getPhaseResults = function(competitionId, phaseId) {
+    Competition.getPhaseResults = function (competitionId, phaseId) {
         $(".competition_results").html("").append("<div class='competitionPreloader'></div>").children().css({ 'top': '200px', 'display': 'block' });
         var url = "/competitions/" + competitionId + "/results/" + phaseId;
         $.ajax({
@@ -205,7 +171,7 @@ var Competition;
         });
     }
 
-    Competition.registationCanProceed = function() {
+    Competition.registationCanProceed = function () {
         if ($("#checkbox").is(":checked") === true) {
             $("#participateButton").removeClass("disabledStatus");
         } else {
@@ -213,7 +179,7 @@ var Competition;
         };
     };
 
-    Competition.displayRegistrationStatus = function() {
+    Competition.displayRegistrationStatus = function () {
         var sOut = "";
         sOut = "<div class='participateInfoBlock pendingApproval'>"
         sOut += "<div class='infoStatusBar'></div>"
@@ -225,8 +191,157 @@ var Competition;
         return sOut;
     }
 
-})(Competition || (Competition = {}));
+    function fmt2(val) {
+        var s = val.toString();
+        if (s.length == 1) {
+            s = "0" + s;
+        }
+        return s;
+    }
+    function lastModifiedLabel(dtString) {
+        var d;
+        if ($.browser.msie && (parseInt($.browser.version) === 8)) {
+            d = new Date();
+            var dd = parseInt(dtString.substring(8, 10), 10);
+            var mm = parseInt(dtString.substring(5, 7), 10);
+            var yr = parseInt(dtString.substring(0, 4), 10);
+            var hh = parseInt(dtString.substring(11, 13), 10);
+            var mn = parseInt(dtString.substring(14, 16), 10);
+            var sc = parseInt(dtString.substring(17, 19), 10);
+            d.setUTCDate(dd);
+            d.setUTCMonth(mm);
+            d.setUTCFullYear(yr);
+            d.setUTCHours(hh);
+            d.setUTCMinutes(mn);
+            d.setUTCSeconds(sc);
+        } else {
+            d = new Date(dtString);
+        }
+        var dstr = $.datepicker.formatDate('M dd, yy', d).toString();
+        var hstr = d.getHours().toString();
+        var mstr = fmt2(d.getMinutes());
+        var sstr = fmt2(d.getSeconds());
+        return "Last modified: " + dstr + " at " + hstr + ":" + mstr + ":" + sstr;
+    }
 
+    Competition.displayNewSubmission = function (response) {
+        var elemTr = $("#submission_details_template #submission_row_template tr").clone();
+        $(elemTr).attr("id", response.id.toString());
+        $(elemTr).addClass(Competition.oddOrEven(response.submission_number));
+        $(elemTr).children().each(function (index) {
+            switch (index) {
+                case 0: if (response.status === 9) { $(this).val("1"); } break;
+                case 1: $(this).html(response.submission_number.toString()); break;
+                case 2:
+                    var fmt = function (val) {
+                        var s = val.toString();
+                        if (s.length == 1) {
+                            s = "0" + s;
+                        }
+                        return s;
+                    }
+                    var dt = new Date(response.submitted_at);
+                    var d = $.datepicker.formatDate('mm/dd/yy', dt).toString();
+                    var h = dt.getHours().toString();
+                    var m = fmt(dt.getMinutes());
+                    var s = fmt(dt.getSeconds());
+                    $(this).html(d + " " + h + ":" + m + ":" + s);
+                    break;
+                case 3: $(this).html(Competition.getSubmissionStatus(response.status)); break;
+            }
+        }
+      );
+        return elemTr;
+    }
+
+    Competition.oddOrEven = function (x) {
+        return (x & 1) ? "odd" : "even";
+    }
+
+    Competition.getSubmissionStatus = function (status) {
+        var subStatus = "Unknown";
+        switch (status) {
+            case 1: subStatus = "Submitted"; break;
+            case 6: subStatus = "Running"; break;
+            case 7: subStatus = "Failed"; break;
+            case 8: subStatus = "Cancelled"; break;
+            case 9: subStatus = "Finished"; break;
+        }
+        return subStatus;
+    }
+
+    Competition.showOrHideSubmissionDetails = function (obj) {
+        var nTr = $(obj).parents('tr')[0];
+        if (prTable.fnIsOpen(nTr)) {
+            $(obj).removeClass("enclosed-foundicon-minus");
+            $(obj).addClass("enclosed-foundicon-plus");
+            prTable.fnClose(nTr);
+        }
+        else {
+            $(obj).removeClass("enclosed-foundicon-plus");
+            $(obj).addClass("enclosed-foundicon-minus");
+            var elem = $("#submission_details_template .submission_details").clone();
+            elem.find("a").each(function (i) { $(this).attr("href", $(this).attr("href").replace("_", nTr.id)) });
+            var state = $(nTr).find("input[name='state']").val();
+            if (state == 1) {
+                var btn = elem.find("button");
+                btn.removeClass("hide");
+                var submitted = $(nTr).find(".status").hasClass("submitted");
+                var competition = $("#competitionId").val();
+                if (submitted) {
+                    btn.addClass("leaderBoardRemove");
+                    btn.text("Remove from Leaderboard");
+                    btn.on('click', function () {
+                        removeFromLeaderboard(competition, nTr.id, cstoken);
+                    });
+                } else {
+                    btn.addClass("leaderBoardSubmit");
+                    btn.text("Submit to Leaderboard");
+                    btn.on('click', function () {
+                        addToLeaderboard(competition, nTr.id, cstoken);
+                    });
+                }
+            }
+            else {
+                var status = $(nTr).find(".statusName").html();
+                var btn = elem.find("button").addClass("hide");
+                if ($.trim(status) === "Submitted" || $.trim(status) === "Running") {
+
+                    btn.removeClass("hide");
+                    btn.text("Refresh status")
+                    btn.on('click', function () {
+                        Competition.updateSubmissionStatus($("#competitionId").val(), nTr.id, this)
+                    });
+                }
+            }
+            prTable.fnOpen(nTr, elem, 'details');
+        }
+    }
+
+    Competition.updateSubmissionStatus = function (competitionId, submissionId, obj) {
+        $(obj).parents(".submission_details").find(".preloader-handler").append("<div class='competitionPreloader'></div>").children().css({ 'top': '25px', 'display': 'block' });
+        var url = "/api/competition/" + competitionId + "/submission/" + submissionId;
+        $.ajax({
+            type: "GET",
+            url: url,
+            cache: false,
+            success: function (data) {
+                $('#user_results #' + submissionId).find(".statusName").html(Competition.getSubmissionStatus(data.status));
+                if (data.status === 9) {
+                    $('#user_results #' + submissionId + "input:hidden").val("1"); $(obj).addClass("leaderBoardSubmit");
+                    $(obj).text("Submit to Leaderboard");
+                }
+                else if (data.status > 6) { $(obj).addClass("hide"); }
+                $(".competitionPreloader").hide();
+            },
+            error: function (xhr, status, err) {
+
+            }
+        });
+    }
+
+})(Competition || (Competition = {}));
+var prTable;
 $(document).ready(function () {
 
     $("#checkbox").click(function (e) { Competition.registationCanProceed(); });
@@ -246,7 +361,6 @@ $(document).ready(function () {
             data: values,
             success: function (response, textStatus, jqXHR) {
                 $('.content form').replaceWith(Competition.displayRegistrationStatus());
-                Competition.invokePhaseButtonOnOpen("submissions_phase_buttons");
             },
             error: function (jsXHR, textStatus, errorThrown) {
                 alert("There was a problem registering for this competition.");;
@@ -266,7 +380,7 @@ $(document).ready(function () {
         });
     });
 
-    $('#Participate').click(function (obj) {
+    $('a[href="#participate-submit_results"]').click(function (obj) {
         Competition.invokePhaseButtonOnOpen("submissions_phase_buttons");
     });
 
@@ -291,3 +405,4 @@ $(document).ready(function () {
     $(".top-bar-section ul > li").removeClass("active");
     $("#liCompetitions").addClass("active");
 });
+
