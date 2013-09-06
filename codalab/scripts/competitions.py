3929b029e1bdef086fc27412bfe997fda457a4ac
Beau Hargis <beau@subobscur.us>
2013-09-06 14:02:54 -0700

    Changes to scoring models and extracting scores

--- codalab/scripts/competitions.py
+++ codalab/scripts/competitions.py
@@ -100,11 +100,12 @@ for g in ({'key': 'patient', 'label': 'Patient Data'},
 	  ):
 	rg,cr = SubmissionResultGroup.objects.get_or_create(competition=brats2012,
 							    key=g['key'],
-							    defaults=dict(label=g['label'],ordering=gorder),
+							    defaults=dict(label=g['label'],
+									  ordering=gorder),
 							    )
 	gorder=gorder+1
 	for gp in brats2012.phases.all():
-		SubmissionResultGroupPhase.objects.create(phase=gp, group=rg)
+		rgp,crx = SubmissionResultGroupPhase.objects.get_or_create(phase=gp, group=rg)
 	groups[rg.key] = rg
 
 for sdg in ( 
@@ -137,50 +138,57 @@ for sdg in (
                                 print "DEFS",defs
                                 
                                 if t == 'computed':
-                                        g,cr = SubmissionScoreGroup.objects.get_or_create(key=defs['key'],competition=brats2012, defaults=dict(label=label))
+                                        g,cr = SubmissionScoreSet.objects.get_or_create(key=defs['key'],
+											competition=brats2012,
+											defaults=dict(label=label))
                                         comp.append((label,defs,g))
                                         print "COMPUTED"
                                 elif t == 'subs':
-                                        g,cr = SubmissionScoreGroup.objects.get_or_create(key="%s%s" % (rgroup,label),
-                                                                                          competition=brats2012, defaults=dict(label=label))
+                                        g,cr = SubmissionScoreSet.objects.get_or_create(key="%s%s" % (rgroup,label),
+											competition=brats2012,
+											defaults=dict(label=label))
                                         for sub in defs:
                                                 print "SUB",sub
                                                 
 
-                                                sd,cr = SubmissionScoreDef.objects.get_or_create(group=groups[rgroup],key=sub[0],
-                                                                                                        defaults=dict(label=sub[1]))
+                                                sd,cr = SubmissionScoreDef.objects.get_or_create(competition=brats2012,key=sub[0],
+												 defaults=dict(label=sub[1]))
+						sdg0,cr = SubmissionScoreDefGroup.objects.get_or_create(scoredef=sd,group=groups[rgroup])
                                                 fields[sd.key] = sd
 
                                                 print " CREATED DEF", sd.key, sd.label
-                                                for p in brats2012.phases.all():
-                                                        sp,cr = SubmissionScorePhase.objects.get_or_create(scoredef=sd,phase=p)
-                                                        print "   ADDED TO PHASE"
+                                                #for p in brats2012.phases.all():
+                                                #        sp,cr = SubmissionScorePhase.objects.get_or_create(scoredef=sd,phase=p)
+                                                #        print "   ADDED TO PHASE"
 
-                                                g2,cr = SubmissionScoreGroup.objects.get_or_create(parent=g,
-                                                                                                   key=sub[0],
-                                                                                                   competition=brats2012,
-                                                                                                   defaults=dict(scoredef=sd,label=sub[1]))
+                                                g2,cr = SubmissionScoreSet.objects.get_or_create(parent=g,
+												 key=sub[0],
+												 competition=brats2012,
+												 defaults=dict(scoredef=sd,label=sub[1]))
                                                 print " SUB GROUP", g2.label,g2.scoredef.key,g2.scoredef.label
 
                                 elif t == 'def':
-                                        g,cr = SubmissionScoreGroup.objects.get_or_create(key="%s%s" % (rgroup,label),
+                                        g,cr = SubmissionScoreSet.objects.get_or_create(key="%s%s" % (rgroup,label),
                                                                                           competition=brats2012, defaults=dict(label=defs[1]))
-                                        sd,cr = SubmissionScoreDef.objects.get_or_create(group=groups[rgroup],
+                                        sd,cr = SubmissionScoreDef.objects.get_or_create(competition=brats2012,
                                                                                          key=defs[0],
                                                                                          defaults = dict(label=defs[1]))
+					sdg0,cr = SubmissionScoreDefGroup.objects.get_or_create(scoredef=sd,group=groups[rgroup])
                                         fields[sd.key] = sd
 
-                                        for p in brats2012.phases.all():
-                                                sp,cr = SubmissionScorePhase.objects.get_or_create(scoredef=sd,phase=p)
+                                        #for p in brats2012.phases.all():
+                                        #        sp,cr = SubmissionScorePhase.objects.get_or_create(scoredef=sd,phase=p)
                                         g.scoredef = sd
                                         g.save()
         for label,defs,g in comp:
-                sd,cr = SubmissionScoreDef.objects.get_or_create(group=groups[rgroup],
+                sd,cr = SubmissionScoreDef.objects.get_or_create(competition=brats2012,
                                                                  key=defs['key'],
                                                                  defaults=dict(label=defs['label']),
                                                                  computed=True)
-                for p in brats2012.phases.all():
-                        SubmissionScorePhase.objects.get_or_create(scoredef=sd,phase=p)
+		sdg0,cr = SubmissionScoreDefGroup.objects.get_or_create(scoredef=sd,group=groups[rgroup])
+
+                #for p in brats2012.phases.all():
+                #        SubmissionScorePhase.objects.get_or_create(scoredef=sd,phase=p)
                 sc,cr = SubmissionComputedScore.objects.get_or_create(scoredef = sd,
                                                                       operation = defs['operation'])
                 for f in defs['fields']:
@@ -188,6 +196,7 @@ for sdg in (
                                                                            scoredef=fields[f])
                 g.scoredef = sd
                 g.save()
+
 # Participant statuses, if they haven't been created before
 statuses = ParticipantStatus.objects.all()
 
