f6303427adf12f12eaa263e51cf1fa56c739ccba
Ivan R. Judson <irjudson@gmail.com>
2013-09-06 13:43:46 -0700

    Working on Competition Editor.

--- codalab/apps/web/forms.py
+++ codalab/apps/web/forms.py
@@ -1,29 +1,24 @@
 from django import forms
 from django.forms.formsets import formset_factory
+# from django.forms.models import inlineformset_factory
 from django.contrib.auth import get_user_model
 import models
 
 User =  get_user_model()
 
 class CompetitionForm(forms.ModelForm):
-    
-    def __init__(self,*args,**kwargs):
-        self._user = kwargs.pop('user',None)
-        super(CompetitionForm,self).__init__(*args,**kwargs)
-
     class Meta:
         model = models.Competition
-        fields = ['title','description','has_registration','end_date']
-
+        fields = ('title', 'description', 'image', 'has_registration', 'end_date')
+        
 class CompetitionPhaseForm(forms.ModelForm):
-
     class Meta:
         model = models.CompetitionPhase
 
-    def save(self,commit=True):
-        #self.request.FILES['dataset']
-        # Possibly need async proccessing
-        return super(CompetitionPhaseForm, self).save(commit)
+    # def save(self, commit=True):
+    #     #self.request.FILES['dataset'] 
+    #     # Possibly need async proccessing
+    #     return super(CompetitionPhaseForm, self).save(commit)
 
 class CompetitionParticipantForm(forms.ModelForm):
     class Meta:
