d797b7e2ab5ee2f0dd33fd770d82eeadc858adf5
Ivan R. Judson <irjudson@gmail.com>
2013-09-13 09:06:27 -0700

    Fixes after merge with master

--- codalab/apps/web/urls/competitions.py
+++ codalab/apps/web/urls/competitions.py
@@ -10,7 +10,5 @@ urlpatterns = patterns('',
                        url(r'^edit_competition/(?P<pk>\d+)$', views.CompetitionEdit.as_view(), name='edit'),
                        url(r'^delete_competition/(?P<pk>\d+)$', views.CompetitionDelete.as_view(), name='delete'),
                        url(r'^(?P<id>\d+)/submissions/(?P<phase>\d+)$', views.CompetitionSubmissionsPage.as_view(), name='competition_submissions_page'),
-                       url(r'^(?P<id>\d+)/results/(?P<phase>\d+)$', views.CompetitionResultsPage.as_view(), name='competition_results_page'),
-                       url(r'^scores/(?P<phase_id>\d+)$',views.ScoresTestView.as_view(template_name='web/competitions/scores.html'),
-                           name='scores_test'),
+                       url(r'^(?P<id>\d+)/results/(?P<phase>\d+)$', views.CompetitionResultsPage.as_view(), name='competition_results_page')
                        )
