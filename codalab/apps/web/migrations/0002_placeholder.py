1c6870d7b69ddcb2540b79222b087d423e12d615
Beau Hargis <beau@subobscur.us>
2013-09-25 15:41:31 -0700

    Added migrations

---
+++ codalab/apps/web/migrations/0002_placeholder.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    depends_on = (
+        ("authenz", "0002_auto__add_cluser"),
+    )
+
+    def forwards(self, orm):
+        pass
+
+    def backwards(self, orm):
+        pass
+
+    models = {
+        
+    }
+
+    complete_apps = ['web']
