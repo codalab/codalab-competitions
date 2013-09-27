819028696ffe4103916ff6a7431a7e6db4ffdb36
Christopher Rampey <csrampey@users.noreply.github.com>
2013-09-26 17:50:28 -0700

    Reorganizing CSS, SASS, JS, and IMG

--- codalab/apps/web/static/config.rb
+++ codalab/apps/web/static/config.rb
@@ -4,8 +4,8 @@
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "img"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
