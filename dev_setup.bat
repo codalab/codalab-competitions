c3018d372f212c14d59abde6a6c124856433e846
Christophe Poulain <cpoulain@live.com>
2013-11-20 13:51:58 -0800

    Rationalize requirements files and adjust one-time setup scripts to address issue #280

--- dev_setup.bat
+++ dev_setup.bat
@@ -15,23 +15,10 @@ REM Activate the virtual environment
 CALL venv\Scripts\activate.bat
 
 REM Update pip and distribute in venv
-pip install --upgrade pip
+pip install pip
 pip install --upgrade distribute
 
 REM Install development requirements
 pip install -r codalab\requirements\dev_azure.txt
 
-cd codalab
-REM If an old database exists, move it out of the way
-if exist dev_db.sqlite move dev_db.sqlite dev_db.sqlite.bak
-
-REM Initialize the app
-python manage.py syncdb --migrate
-python scripts\initialize.py
-
-REM Make test users
-python scripts\users.py
-REM Make example challenges
-python scripts\competitions.py
-
-ECHO "You are now ready to run: python codalab\manage.py runserver"
+ECHO "One time setup is complete. You are ready to proceed."
