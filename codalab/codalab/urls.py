c38a1f849a1b780527633859eec168c142891d66
irjudson <irjudson@gmail.com>
2013-11-27 15:37:54 -0800

    Fixed urls.py

--- codalab/codalab/urls.py
+++ codalab/codalab/urls.py
@@ -1,30 +1,30 @@
 from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'', include('apps.web.urls')),
-    url(r'^accounts/', include('allauth.urls')),
-    url(r'^search/', include('haystack.urls')),
-    url(r'^api/', include('apps.api.routers')),
-    
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       url(r'', include('apps.web.urls')),
+                       url(r'^accounts/', include('allauth.urls')),
+                       url(r'^search/', include('haystack.urls')),
+                       url(r'^api/', include('apps.api.routers')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+                       # Uncomment the admin/doc line below to enable admin
+                       # documentation:
+                       #url(r'^admin/doc/',
+                       #    include('django.contrib.admindocs.urls')),
 
-    # Static files
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.STATIC_ROOT}),
-    # Media files
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.MEDIA_ROOT}),
-    # JS Reverse for saner AJAX calls
-    url(r'^jsreverse/$', 'django_js_reverse.views.urls_js', name='js_reverse')
-) 
+                       # Uncomment the next line to enable the admin:
+                       #url(r'^admin/', include(admin.site.urls)),
+
+                       # Static files
+                       url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+                           {'document_root': settings.STATIC_ROOT}),
+                       # Media files
+                       url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                           {'document_root': settings.MEDIA_ROOT}),
+                       # JS Reverse for saner AJAX calls
+                       url(r'^jsreverse/$',
+                           'django_js_reverse.views.urls_js', name='js_reverse')
+                       )
