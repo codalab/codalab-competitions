b9008967cc9f61ac7f7ef4fe76ee1b7a4734bf28
Christophe Poulain <cpoulain@live.com>
2014-02-22 21:39:42 -0800

    Add OAuth2 authorization server and token validation API for bundle server.

---
+++ codalab/apps/authenz/oauth.py
@@ -0,0 +1,64 @@
+"""
+Implements CodaLab-specific requirements for OAuth.
+"""
+
+import logging
+
+from oauth2_provider.models import Application
+from oauth2_provider.oauth2_validators import OAuth2Validator
+
+logger = logging.getLogger(__name__)
+
+#
+# Customized OAuth validator
+#
+
+class Validator(OAuth2Validator):
+    """
+    Customizes the default validator.
+    """
+
+    def validate_scopes(self, client_id, scopes, client, request, *args, **kwargs):
+        """
+        Ensure required scopes are permitted (as specified in the settings file)
+        """
+        allowed_scopes = []
+        if client.authorization_grant_type == Application.GRANT_CLIENT_CREDENTIALS:
+            allowed_scopes = ['token-validation']
+        return set(scopes).issubset(allowed_scopes)
+
+    def get_default_scopes(self, client_id, request, *args, **kwargs):
+        return []
+
+#
+# Helpers to create a default client to allow any CodaLab user to use their web site credentials
+# to connect their CLI client tool to the bundle service via OAuth protocol.
+#
+
+def cli_client_id(user):
+    """Provides the default CLI Client ID associated with the given user in the OAuth provider store."""
+    return 'cli_client_{0}'.format(user.username)
+
+def get_or_create_cli_client(user):
+    """
+    Get or create a default OAuth client that the given user can use to authorize his CLI application.
+    """
+    client_id = cli_client_id(user)
+    try:
+        logger.debug("Attempting to create cli client %s", client_id)
+        client, created = Application.objects.get_or_create(
+                            client_id=client_id,
+                            user=user,
+                            client_type=Application.CLIENT_CONFIDENTIAL,
+                            authorization_grant_type=Application.GRANT_PASSWORD,
+                            client_secret='',
+                            name=client_id)
+        if created:
+            logger.info("Created CLI client %s", client_id)
+        else:
+            logger.info("CLI client %s existed already", client_id)
+        return client, created
+    except:
+        logger.exception("Failed to create CLI client %s.", client_id)
+        return None, False
+
