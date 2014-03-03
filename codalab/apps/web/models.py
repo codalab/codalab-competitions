27ad3e07100edb81f1b57ddcf36ad448c224e061
Christopher Rampey <csrampey@users.noreply.github.com>
2014-03-03 13:50:45 -0800

    Fixed submit/view text that I missed in last commit

--- codalab/apps/web/models.py
+++ codalab/apps/web/models.py
@@ -783,7 +783,7 @@ class CompetitionDefBundle(models.Model):
         Page.objects.create(category=participate_category, container=pc,  codename="get_data",
                                    label="Get Data", rank=0, html=zf.read(comp_spec['html']['data']))
 
-        Page.objects.create(category=participate_category, container=pc,  codename="submit_results", label="Submit/View Results", rank=1, html="")
+        Page.objects.create(category=participate_category, container=pc,  codename="submit_results", label="Submit / View Results", rank=1, html="")
 
         logger.debug("CompetitionDefBundle::unpack created competition pages (pk=%s)", self.pk)
 
