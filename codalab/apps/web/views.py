8bb0114d4d17e7000b9a60bae81fa53ecfc1c636
Javier Luraschi <javierp@outlook.com>
2014-03-10 09:05:46 -0700

    Improve admin scores and participants

--- codalab/apps/web/views.py
+++ codalab/apps/web/views.py
@@ -49,6 +49,14 @@ def my_index(request):
         })
     return HttpResponse(template.render(context))
 
+def sort_data_table(request, context, list):
+    context['order'] = order = request.GET.get('order') if 'order' in request.GET else 'id'
+    context['direction'] = direction = request.GET.get('direction') if 'direction' in request.GET else 'asc'
+    reverse = direction == 'desc'
+    def sortkey(x):
+        return x[order] if order in x and x[order] is not None else ''
+    list.sort(key=sortkey, reverse=reverse)
+
 class LoginRequiredMixin(object):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
@@ -179,6 +187,8 @@ class CompetitionResultsPage(TemplateView):
         context = super(CompetitionResultsPage, self).get_context_data(**kwargs)
         competition = models.Competition.objects.get(pk=self.kwargs['id'])
         phase = competition.phases.get(pk=self.kwargs['phase'])
+        is_owner = self.request.user.id == competition.creator_id
+        context['is_owner'] = is_owner
         context['phase'] = phase
         context['groups'] = phase.scores()
         return context
@@ -242,9 +252,47 @@ class MyCompetitionParticipantView(LoginRequiredMixin, ListView):
     template_name = 'web/my/participants.html'
 
     def get_context_data(self, **kwargs):
-        ctx = super(MyCompetitionParticipantView, self).get_context_data(**kwargs)
-        ctx['competition_id'] = self.kwargs.get('competition_id')
-        return ctx
+        context = super(MyCompetitionParticipantView, self).get_context_data(**kwargs)
+        # create column definition
+        columns = [
+            {
+                'label': 'NAME',
+                'name': 'name'
+            },
+            {
+                'label': 'EMAIL',
+                'name': 'email'
+            },
+            {
+                'label': 'STATUS',
+                'name': 'status'
+            },
+            {
+                'label': 'ENTRIES',
+                'name': 'entries'
+            }
+        ]
+        context['columns'] = columns
+        # retrieve participant submissions information
+        participant_list = []
+        competition_participants = self.queryset.filter(competition=self.kwargs.get('competition_id'))
+        competition_participants_ids = list(participant.id for participant in competition_participants)
+        context['pending_participants'] = filter(lambda participant_submission: participant_submission.status.codename == models.ParticipantStatus.PENDING, competition_participants)
+        participant_submissions = models.CompetitionSubmission.objects.filter(participant__in=competition_participants_ids)
+        for participant in competition_participants:
+            participant_entry = {
+                'name': participant.user.username,
+                'email': participant.user.email,
+                'status': participant.status.codename,
+                # equivalent to assigning participant.submissions.count() but without several multiple db queires
+                'entries': len(filter(lambda participant_submission: participant_submission.participant.id == participant.id, participant_submissions))
+            }
+            participant_list.append(participant_entry)
+        # order results
+        sort_data_table(self.request, context, participant_list)
+        context['participant_list'] = participant_list
+        context['competition_id'] = self.kwargs.get('competition_id')
+        return context
 
     def get_queryset(self):
         return self.queryset.filter(competition=self.kwargs.get('competition_id'))
@@ -349,25 +397,50 @@ class MyCompetitionSubmissionsPage(LoginRequiredMixin, TemplateView):
         context = super(MyCompetitionSubmissionsPage, self).get_context_data(**kwargs)
         competition = models.Competition.objects.get(pk=self.kwargs['competition_id'])
         context['competition'] = competition
-        context['order'] = order = self.request.GET.get('order') if 'order' in self.request.GET else 'id'
-        context['direction'] = direction = self.request.GET.get('direction') if 'direction' in self.request.GET else 'asc'
         if self.request.user.id == competition.creator_id:
+            # find the active phase
             if (phase_id != None):
                 context['selected_phase_id'] = int(phase_id)
-                activePhase = competition.phases.filter(id=phase_id)
+                active_phase = competition.phases.filter(id=phase_id)[0]
             else:
-                activePhase = competition.phases.all()[0]
+                active_phase = competition.phases.all()[0]
                 for phase in competition.phases.all():
                     if phase.is_active:
                         context['selected_phase_id'] = phase.id
-                        activePhase = phase
-
-            submissions = models.CompetitionSubmission.objects.filter(phase=activePhase)
-            # find which submission is in the leaderboard, if any and only if phase allows seeing results.
-            id_of_submission_in_leaderboard = -1
-            ids = [e.result.id for e in models.PhaseLeaderBoardEntry.objects.all()
-                                if e.result in submissions]
-            if len(ids) > 0: id_of_submission_in_leaderboard = ids[0]
+                        active_phase = phase
+            submissions = models.CompetitionSubmission.objects.filter(phase=active_phase)
+            # find which submissions are in the leaderboard, if any and only if phase allows seeing results.
+            id_of_submissions_in_leaderboard = [e.result.id for e in models.PhaseLeaderBoardEntry.objects.all() if e.result in submissions]
+            # create column definition
+            columns = [
+                {
+                    'label': 'SUBMITTED',
+                    'name': 'submitted_at'
+                },
+                {
+                    'label': 'SUBMITTED BY',
+                    'name': 'submitted_by'
+                },
+                {
+                    'label': 'FILENAME',
+                    'name': 'filename'
+                },
+                {
+                    'label': 'STATUS',
+                    'name': 'status_name'
+                },
+                {
+                    'label': 'LEADERBOARD',
+                    'name': 'is_in_leaderboard'
+                },
+            ]
+            scores = active_phase.scores()
+            for score_group_index, score_group in enumerate(scores):
+                column = {
+                    'label': score_group['label'],
+                    'name': 'score_' + str(score_group_index),
+                }
+                columns.append(column)
             # map submissions to view data
             submission_info_list = []
             for submission in submissions:
@@ -378,13 +451,19 @@ class MyCompetitionSubmissionsPage(LoginRequiredMixin, TemplateView):
                     'filename': submission.get_filename(),
                     'submitted_at': submission.submitted_at,
                     'status_name': submission.status.name,
-                    'is_in_leaderboard': submission.id == id_of_submission_in_leaderboard
+                    'is_in_leaderboard': submission.id in id_of_submissions_in_leaderboard
                 }
+                # add score groups into data columns
+                if (submission_info['is_in_leaderboard'] == True):
+                    for score_group_index, score_group in enumerate(scores):
+                        user_score = filter(lambda user_score: user_score[1]['username'] == submission.participant.user.username, score_group['scores'])[0]
+                        main_score = filter(lambda main_score: main_score['name'] == score_group['selection_key'], user_score[1]['values'])[0]
+                        submission_info['score_' + str(score_group_index)] = main_score['val']
                 submission_info_list.append(submission_info)
-            reverse = direction == 'desc'
-            def mysortkey(x):
-                return x[order]
-            submission_info_list.sort(key=mysortkey, reverse=reverse)
+            # order results
+            sort_data_table(self.request, context, submission_info_list)
+            # complete context
+            context['columns'] = columns
             context['submission_info_list'] = submission_info_list
         return context
 
