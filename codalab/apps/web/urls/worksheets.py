c0ad49ce9ed56faf1b6c4818c22f54309a0ce949
Javier Luraschi <javierp@outlook.com>
2014-04-03 13:48:25 -0700

    Implement worksheet lists (using Angular)

--- codalab/apps/web/urls/worksheets.py
+++ codalab/apps/web/urls/worksheets.py
@@ -5,6 +5,6 @@ from django.views.generic import TemplateView
 from .. import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.WorksheetListView.as_view(), name='list'),
+    url(r'^$', 'django.contrib.staticfiles.views.serve', kwargs={'path': '../static/app/index.html'}),
     url(r'^(?P<uuid>[A-Za-z0-9]+)$', views.WorksheetDetailView.as_view(), name='view'),
 )
