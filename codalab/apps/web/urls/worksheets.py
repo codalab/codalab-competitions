3799b3e4d9ce106c96df41e9a85efacd6be8fee6
cpoulain <cxpoulain@live.com>
2014-05-30 11:28:58 -0700

    Undo changes.

--- codalab/apps/web/urls/worksheets.py
+++ codalab/apps/web/urls/worksheets.py
@@ -6,6 +6,5 @@ from .. import views
 
 urlpatterns = patterns('',
     url(r'^$', views.WorksheetListView.as_view(), name='list'),
-    #url(r'^$', 'django.contrib.staticfiles.views.serve', kwargs={'path': '../static/app/index.html'}),
     url(r'^(?P<uuid>[A-Za-z0-9]+)$', views.WorksheetDetailView.as_view(), name='view'),
 )
