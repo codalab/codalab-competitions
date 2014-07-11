b9f06ea64765469a303f3f5706cd96c162fd9586
Eric Carmichael <eric@ckcollab.com>
2014-07-10 18:53:02 -0700

    woo 2 competitions pointing to same OrganizerDataSet

---
+++ codalab/apps/web/tests/test_notifications.py
@@ -0,0 +1,212 @@
+import mock
+
+from django.conf import settings
+from django.core import management
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from django.core import mail
+from django.test.client import Client
+from django.contrib.auth import get_user_model
+
+from pytz import utc
+
+from apps.web.models import (Competition,
+                             CompetitionDefBundle,
+                             CompetitionParticipant,
+                             CompetitionPhase,
+                             CompetitionSubmission,
+                             CompetitionSubmissionStatus,
+                             ParticipantStatus,
+                             PhaseLeaderBoard,
+                             PhaseLeaderBoardEntry)
+from apps.web import tasks
+
+User = get_user_model()
+
+
+class CompetitionTest(TestCase):
+
+    def setUp(self):
+        self.organizer_user = User.objects.create_user(username="organizer", password="pass")
+        self.participant_user = User.objects.create_user(username="participant", password="pass")
+        self.competition = Competition.objects.create(
+            title="Test Competition",
+            creator=self.organizer_user,
+            modified_by=self.organizer_user
+        )
+        self.participant = CompetitionParticipant.objects.create(
+            user=self.participant_user,
+            competition=self.competition,
+            status=ParticipantStatus.objects.get_or_create(name='approved', codename=ParticipantStatus.PENDING)[0]
+        )
+
+
+class CompetitionMessageParticipantsTests(CompetitionTest):
+
+    def test_msg_participants_view_returns_400_on_get(self):
+        self.client.login(username="organizer", password="pass")
+        resp = self.client.get(
+            reverse("competitions:competition_message_participants", kwargs={"competition_id": self.competition.pk})
+        )
+        self.assertEquals(resp.status_code, 400)
+
+    def test_msg_participants_view_returns_200_on_valid_POST_and_works(self):
+        self.client.login(username="organizer", password="pass")
+        with mock.patch('apps.web.tasks.send_mass_email') as send_mass_email_mock:
+            resp = self.client.post(
+                reverse("competitions:competition_message_participants", kwargs={"competition_id": self.competition.pk}),
+                data={"subject": "test", "body": "message body"}
+            )
+            self.assertEquals(resp.status_code, 200)
+        self.assertTrue(send_mass_email_mock.called)
+
+    def test_msg_participants_view_returns_400_on_no_data(self):
+        self.client.login(username="organizer", password="pass")
+        resp = self.client.post(
+            reverse("competitions:competition_message_participants", kwargs={"competition_id": self.competition.pk})
+        )
+
+        self.assertEquals(resp.status_code, 400)
+        self.assertIn("Missing subject or body of message!", str(resp))
+
+    def test_msg_participants_view_returns_400_on_bad_data(self):
+        self.client.login(username="organizer", password="pass")
+        resp = self.client.post(
+            reverse("competitions:competition_message_participants", kwargs={"competition_id": self.competition.pk}),
+            data={"not_right": "bad data"}
+        )
+
+        self.assertEquals(resp.status_code, 400)
+        self.assertIn("Missing subject or body of message!", str(resp))
+
+    def test_msg_participants_view_returns_403_when_not_competition_owner(self):
+        self.client.login(username="participant", password="pass")
+        resp = self.client.post(
+            reverse("competitions:competition_message_participants", kwargs={"competition_id": self.competition.pk})
+        )
+        self.assertEquals(resp.status_code, 403)
+
+    def test_msg_participants_view_returns_404_when_competition_doesnt_exist(self):
+        self.client.login(username="organizer", password="pass")
+        non_existant_competition_id = 2398234
+        resp = self.client.post(
+            reverse("competitions:competition_message_participants", kwargs={"competition_id": non_existant_competition_id})
+        )
+        self.assertEquals(resp.status_code, 404)
+
+    def test_msg_participants_view_returns_302_when_not_logged_in(self):
+        resp = self.client.post(
+            reverse("competitions:competition_message_participants", kwargs={"competition_id": self.competition.pk})
+        )
+        self.assertEquals(resp.status_code, 302)
+
+    def test_msg_participants_task_called_with_proper_args(self):
+        self.client.login(username="organizer", password="pass")
+        with mock.patch('apps.web.tasks.send_mass_email') as send_mass_email_mock:
+            resp = self.client.post(
+                reverse("competitions:competition_message_participants", kwargs={"competition_id": self.competition.pk}),
+                data={"subject": "test", "body": 'Injected!<script src="http://code_injection/bad_code.js"></script>'}
+            )
+            self.assertEquals(resp.status_code, 200)
+
+        send_mass_email_mock.assert_called_with(
+            self.competition,
+            body=u'Injected!', # <script> tag was removed!
+            subject=u'test',
+            to_emails=[self.participant_user.email],
+            from_email=settings.DEFAULT_FROM_EMAIL
+        )
+
+    def test_msg_participants_email_not_sent_when_participant_disables_organizer_emails(self):
+        self.participant_user.organizer_direct_message_updates = False
+        self.participant_user.save()
+
+        self.client.login(username="organizer", password="pass")
+
+        with mock.patch('apps.web.tasks.send_mass_email') as send_mass_email_mock:
+            resp = self.client.post(
+                reverse("competitions:competition_message_participants", kwargs={"competition_id": self.competition.pk}),
+                data={"subject": "test", "body": 'Test body'}
+            )
+            self.assertEquals(resp.status_code, 200)
+
+        self.assertFalse(send_mass_email_mock.called)
+
+
+class AccountSettingsTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="user", password="pass", email="user@test.com")
+        self.client.login(username="user", password="pass")
+
+    def test_account_settings_view_returns_200(self):
+        resp = self.client.get(reverse("user_settings"))
+        self.assertEquals(resp.status_code, 200)
+
+    def test_account_settings_view_returns_403_when_not_logged_in(self):
+        self.client.logout()
+        resp = self.client.get(reverse("user_settings"))
+        self.assertEquals(resp.status_code, 302)
+
+    def test_account_settings_view_returns_200_on_valid_POST(self):
+        resp = self.client.post(
+            reverse("user_settings"),
+            {
+                'participation_status_updates': True,
+                'organizer_status_updates': False,
+                'organizer_direct_message_updates': True
+            }
+        )
+        self.assertEquals(resp.status_code, 200)
+
+        updated_user = User.objects.get(pk=self.user.pk)
+        self.assertFalse(updated_user.organizer_status_updates)
+
+    def test_account_settings_view_returns_400_on_no_data(self):
+        resp = self.client.post(
+            reverse("user_settings"),
+        )
+        self.assertEquals(resp.status_code, 400)
+
+    def test_account_settings_view_returns_400_on_bad_data(self):
+        resp = self.client.post(
+            reverse("user_settings"),
+            {
+                "test": "test"
+            }
+        )
+        self.assertEquals(resp.status_code, 400)
+
+
+class SendMassEmailTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="user", password="pass", email="user@test.com")
+        self.competition = Competition.objects.create(creator=self.user, modified_by=self.user)
+
+    def test_send_mass_email_works(self):
+        task_args = {
+            "competition": self.competition,
+            "body": "Body",
+            "subject": "Subject",
+            "from_email": "no-reply@test.com",
+            "to_emails": ["%s@test.com" % i for i in range(0, 10)]
+        }
+        tasks.send_mass_email_task(1, task_args)
+
+        self.assertEquals(len(mail.outbox), 10)
+
+    def test_send_mass_email_has_valid_links(self):
+
+        task_args = {
+            "competition": self.competition,
+            "body": "Body",
+            "subject": "Subject",
+            "from_email": "no-reply@test.com",
+            "to_emails": ["test@test.com"]
+        }
+        tasks.send_mass_email_task(1, task_args)
+
+        m = mail.outbox[0]
+        self.assertIn("http://example.com/my/settings", m.body)
+        self.assertIn("http://example.com/competitions/%s" % self.competition.pk, m.body)
