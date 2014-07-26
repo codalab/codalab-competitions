23fd75afab74b5877ee892c18e85fdf1dd1db2ff
Xavier Baró <xbaro@uoc.edu>
2014-07-26 18:06:12 +0200

    User Model extended
    Added extra fields to the default user model

--- codalab/apps/authenz/models.py
+++ codalab/apps/authenz/models.py
@@ -3,4 +3,10 @@ from django.contrib.auth import models as auth_models
 
 
 class ClUser(auth_models.AbstractUser):
-    pass
+
+    # Additional attributes for user profile.
+    affiliation = models.CharField(max_length=200,blank=True)
+    location = models.CharField(max_length=200,blank=True) # Use CountryField or more complex model (https://djangosnippets.org/snippets/912/)
+    picture = models.ImageField(upload_to='profile_images', blank=True, default='') # Can be stored in Azure. Put a default image for profiles?
+    biography= models.TextField(blank=True)
+    website = models.URLField(blank=True)
