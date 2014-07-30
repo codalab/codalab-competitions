f57c35feca0dab457bf8e6fecc88c6a3b7596027
Eric Carmichael <eric@ckcollab.com>
2014-07-30 09:48:13 -0700

    removes the behavior that hid the 'submit to leaderboard' button, reverting back to old behavior

--- codalab/apps/web/static/js/Competition.js
+++ codalab/apps/web/static/js/Competition.js
@@ -13,22 +13,16 @@ var Competition;
     };
 
     function decorateLeaderboardButton(btn, submitted) {
-        var force_submission_to_leaderboard = btn.attr('force_submission_to_leaderboard');
+        //var force_submission_to_leaderboard = btn.attr('force_submission_to_leaderboard');
 
-        if (force_submission_to_leaderboard) {
-            if (submitted) {
-                btn.text('Automatically submitted to leaderboard').attr('disabled', 'disabled');
-            }
+        if (submitted) {
+            btn.removeClass('leaderBoardSubmit');
+            btn.addClass('leaderBoardRemove');
+            btn.text('Remove from Leaderboard');
         } else {
-            if (submitted) {
-                btn.removeClass('leaderBoardSubmit');
-                btn.addClass('leaderBoardRemove');
-                btn.text('Remove from Leaderboard');
-            } else {
-                btn.removeClass('leaderBoardRemove');
-                btn.addClass('leaderBoardSubmit');
-                btn.text('Submit to Leaderboard');
-            }
+            btn.removeClass('leaderBoardRemove');
+            btn.addClass('leaderBoardSubmit');
+            btn.text('Submit to Leaderboard');
         }
     }
 
@@ -386,17 +380,11 @@ var Competition;
                     $('#user_results #' + submissionId + 'input:hidden').val('1');
                     var phasestate = $('#phasestate').val();
                     if (phasestate == 1) {
-                        var force_submission_to_leaderboard = $(obj).attr('force_submission_to_leaderboard');
-
-                        if (!force_submission_to_leaderboard) {
-                            $(obj).addClass('leaderBoardSubmit');
-                            $(obj).text('Submit to Leaderboard');
-                            $(obj).on('click', function() {
-                                updateLeaderboard(competitionId, submissionId, $('#cstoken').val(), $(obj));
-                            });
-                        } else {
-                            $(obj).text('Automatically submitted to leaderboard').attr('disabled', 'disabled');
-                        }
+                        $(obj).addClass('leaderBoardSubmit');
+                        $(obj).text('Submit to Leaderboard');
+                        $(obj).on('click', function() {
+                            updateLeaderboard(competitionId, submissionId, $('#cstoken').val(), $(obj));
+                        });
                     } else {
                         $(obj).addClass('hide');
                     }
