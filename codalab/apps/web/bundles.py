502e7623fcc895511a0a74488a7a63dc09ae89a8
Francis <francis@tivix.com>
2014-08-04 15:59:24 -0700

    added interpreted to worksheet call to get print version.

--- codalab/apps/web/bundles.py
+++ codalab/apps/web/bundles.py
@@ -9,9 +9,9 @@ if len(settings.BUNDLE_SERVICE_URL) > 0:
     from codalab.common import UsageError
     from codalab.client.remote_bundle_client import RemoteBundleClient
     from apps.authenz.oauth import get_user_token
+    from codalab.lib import worksheet_util
 
     def _call_with_retries(f, retry_count=0):
-
         try:
             return f()
         except (Fault, ProtocolError) as e:
@@ -39,8 +39,17 @@ if len(settings.BUNDLE_SERVICE_URL) > 0:
         def create_worksheet(self, name):
             return _call_with_retries(lambda: self.client.new_worksheet(name))
 
-        def worksheet(self, uuid):
-            return _call_with_retries(lambda: self.client.get_worksheet_info(uuid))
+        def worksheet(self, uuid, interpreted=False):
+            worksheet_info  = _call_with_retries(lambda: self.client.get_worksheet_info(uuid))
+            if interpreted:
+                interpreted = worksheet_util.interpret_items(
+                                    worksheet_util.get_default_schemas(),
+                                    worksheet_info['items']
+                                )
+                interpreted['owner_id'] = worksheet_info['owner_id']
+                return interpreted
+            else:
+                return worksheet_info
 
         def ls(self, uuid, path):
             return _call_with_retries(lambda: self.client.ls((uuid, path)))
