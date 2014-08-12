a13c0babbf3a9dda4f9ae46f4a066d8d2bfcdd65
Eric Carmichael <eric@ckcollab.com>
2014-08-12 10:33:47 -0700

    getting jobs in queue, saving spot

--- codalab/apps/health/views.py
+++ codalab/apps/health/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from apps.jobs.models import Job
+from apps.web import models as web_models
+
 
 def health(request):
+    jobs_in_queue_length = Job.objects.filter(status=Job.PENDING)
 
-    return render(request, "health/health.html")
+
+    return render(request, "health/health.html", {
+
+    })
