4746292f0a01dca34e9698fa8eeb3feb9d621316
Francis <francis@tivix.com>
2014-08-19 12:09:25 -0700

    added link to bundle page and fixed / at ulrs

--- codalab/apps/web/urls/bundles.py
+++ codalab/apps/web/urls/bundles.py
@@ -6,5 +6,5 @@ from apps.web import views
 
 urlpatterns = patterns('',
     url(r'^$', views.BundleListView.as_view(), name='bundle_list'),
-    url(r'^(?P<uuid>[A-Za-z0-9]+)$', views.BundleDetailView.as_view(), name="bundle_detail"),
+    url(r'^(?P<uuid>[-A-Za-z0-9_]+)/$', views.BundleDetailView.as_view(), name="bundle_detail"),
 )
