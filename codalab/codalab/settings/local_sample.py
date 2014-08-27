ea11fac45ebe1467197895e2322fc9e39fbcfb54
Percy Liang <percyliang@gmail.com>
2014-08-26 22:40:42 -0700

    default to sqlite3 instead of mysql

--- codalab/codalab/settings/local_sample.py
+++ codalab/codalab/settings/local_sample.py
@@ -48,12 +48,15 @@ class Dev(DevBase):
 
     DATABASES = {
         'default': {
-            'ENGINE':  'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'MySQL_DevDB',                 # Or path to database file if using sqlite3.
-            # The following settings are not used with sqlite3:
-            'USER': 'someuser',
-            'PASSWORD': 'somepassword',
-            'HOST': 'someserver', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-            'PORT': '',           # Set to empty string for default.
+            'ENGINE': 'django.db.backends.sqlite3', # Simple database
+            'NAME': 'codalab.sqlite3',              # Path to database file
+
+            # If want something fancier:
+            #'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            #'NAME': 'MySQL_DevDB',                # Or path to database file if using sqlite3.
+            #'USER': 'someuser',
+            #'PASSWORD': 'somepassword',
+            #'HOST': 'someserver', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+            #'PORT': '',           # Set to empty string for default.
         }
     }
