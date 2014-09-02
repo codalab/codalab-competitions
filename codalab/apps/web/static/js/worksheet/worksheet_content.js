84c2362059943174337d9822b63e1642ed6608e0
Adam Lord <adam.lord@gmail.com>
2014-09-02 11:18:04 -0700

    #491
    - refactor Worksheet class and markup bundle consolidation to store state in global ws_obj object
    - add WorksheetItem class
    - markup bundles are editable and ready to be saved
    - update MathJax cdn address per http://www.mathjax.org/changes-to-the-mathjax-cdn/

--- codalab/apps/web/static/js/worksheet/worksheet_content.js
+++ codalab/apps/web/static/js/worksheet/worksheet_content.js
@@ -10,37 +10,34 @@ var WorksheetContent = function() {
             owner: null,
             owner_id: 0,
             uuid: 0,
-            items: [ ]
-        };
-        this.consolidateMarkdownBundles = function(ws) {
-            var consolidatedWorksheet = [];
-            var markdownChunk         = '';
-            ws.items.map(function(item){
-                var mode        = item['mode'];
-                var interpreted = item['interpreted'];
-                switch(mode) {
-                    case 'markup':
-                        var content = interpreted + '\n';
-                        markdownChunk += content;
-                        break;
-                    default:
-                        if(markdownChunk.length){
-                            consolidatedWorksheet.push({
-                                mode: 'markup',
-                                interpreted: markdownChunk,
-                                bundle_info: null
-                            });
-                            markdownChunk = '';
-                        }
-                        consolidatedWorksheet.push(item);
-                        break;
-                }
-            });
-            ws.items = consolidatedWorksheet;
-            return ws;
+            items: []
         };
     }
     //add functions and calls below
+    WorksheetContent.prototype.consolidateMarkdownBundles = function(ws_items) {
+        var consolidatedWorksheet = [];
+        var markdownChunk         = '';
+        ws_items.map(function(item, index){
+            var mode        = item.state.mode;
+            var interpreted = item.state.interpreted;
+            if(mode == 'markup' && index <= ws_items.length - 1){
+                var content = interpreted + '\n';
+                markdownChunk += content;
+                if(index == ws_items.length - 1){
+                    newMarkdownItem = new WorksheetItem(markdownChunk, item.state.bundle_info, 'markup');
+                    consolidatedWorksheet.push(newMarkdownItem);
+                }
+            }else {
+                if(markdownChunk.length){
+                    newMarkdownItem = new WorksheetItem(markdownChunk, item.state.bundle_info, 'markup');
+                    consolidatedWorksheet.push(newMarkdownItem);
+                    markdownChunk = '';
+                }
+                consolidatedWorksheet.push(item);
+            }
+        });
+        return consolidatedWorksheet;
+    };
     WorksheetContent.prototype.fetch = function(props) {
         props = props || {};
         props.success = props.success || function(data){};
@@ -55,9 +52,15 @@ var WorksheetContent = function() {
                 console.log("WorksheetContent: setting worksheet state:");
                 console.log(data);
                 console.log('');
-                consolidatedWorksheet = this.consolidateMarkdownBundles(data);
-                this.state = consolidatedWorksheet;
-                props.success(consolidatedWorksheet);
+                ws_obj.state = data;
+                var ws_items = [];
+                data.items.map(function(item){
+                    var ws_item = new WorksheetItem(item.interpreted, item.bundle_info, item.mode);
+                    ws_items.push(ws_item);
+                });
+                consolidatedWorksheetItems = this.consolidateMarkdownBundles(ws_items);
+                ws_obj.state.items = consolidatedWorksheetItems;
+                props.success(consolidatedWorksheetItems);
             }.bind(this),
             error: function(xhr, status, err) {
                 props.error(xhr, status, err);
@@ -65,4 +68,20 @@ var WorksheetContent = function() {
         });
     };
     return WorksheetContent;
+}();
+
+
+// 'class' to manage a worksheet's items
+// gets created in worksheet/details.html
+var WorksheetItem = function() {
+    //init
+    function WorksheetItem(interpreted, bundle_info, mode) {
+        this.state = {
+            interpreted: interpreted,
+            bundle_info: bundle_info,
+            mode: mode,
+        };
+    }
+   
+    return WorksheetItem;
 }();
