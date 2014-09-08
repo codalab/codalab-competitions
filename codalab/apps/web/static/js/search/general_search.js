1e7561cc4092b76b4fa41c67170b5e932adb5a00
Adam Lord <adam.lord@gmail.com>
2014-09-08 14:28:00 -0700

    Add comments to search bar/event binding functionality

--- codalab/apps/web/static/js/search/general_search.js
+++ codalab/apps/web/static/js/search/general_search.js
@@ -1,5 +1,7 @@
 /** @jsx React.DOM */
 
+// Dictionary of terms that can be entered into the search bar and the names of 
+// functions they call. See search_actions.js
 var fakedata = {
     red: 'doRed',
     green: 'doGreen',
@@ -9,27 +11,25 @@ var fakedata = {
     save: 'doSave'
     };
 
+
 var Search = React.createClass({
-    getInitialState: function(){
-        return {
-            value: '',
-            open: false,
-            results: []
-        };
-    },
     componentDidMount: function(){
+        // when the component has mounted, init the select2 plugin on the 
+        // general search input (http://ivaynberg.github.io/select2/)
         _this = this;
         $('#search').select2({
             multiple:true,
-            tags: function(){
+            tags: function(){ // make a list possible search results from the dict
                 options = [];
-                for(key in fakedata){
+                for(var key in fakedata){
                     options.push(key);
                 }
                 return options;
             },
-            tokenSeparators: [":", " ", ","],
-            createSearchChoicePosition: 'bottom'
+            tokenSeparators: [":", " ", ","], // Define token separators for the tokenizer function
+            createSearchChoicePosition: 'bottom' // Users can enter their own commands (not autocompleted) 
+                                                 // but these will show up at the bottom. This allows a user
+                                                 // to hit 'tab' to select the first highlighted option
         })
         .on('select2-focus', function(){
             _this.handleFocus();
@@ -38,13 +38,20 @@ var Search = React.createClass({
             _this.handleBlur();
         });
         $('#s2id_search').on('keydown', '.select2-input', function(e){
+            // add some custom key events for working with the search bar
             switch(e.keyCode){
                 case 9:
+                    // usually the Tab key would move focus off the search input, so
+                    // we want to prevent that
                     e.preventDefault();
                     break;
                 case 13:
-                    e.preventDefault();
-                    _this.executeCommands();
+                    // cmd-enter or ctrl-enter triggers execution of whatever is
+                    // in the search input
+                    if(e.ctrlKey || e.metaKey){
+                        e.preventDefault();
+                        _this.executeCommands();
+                    }
                     break;
                 default:
                     return true;
@@ -52,24 +59,32 @@ var Search = React.createClass({
         });
     },
     executeCommands: function(){
-        var command = $('#search').select2('val');
+        // parse and execute the contents of the search input
+        var command = $('#search').select2('val'); // this comes in as an array
+        // customization can be done here, depending on the desired syntax of commands.
+        // currently, this just calls all of the functions named in the input
         for(i=0; i < command.length; i++){
-            var functionName = fakedata[command[i]];
-            if(ws_searchActions.hasOwnProperty(functionName)){
-                ws_searchActions[functionName]();
+            var input = fakedata[command[i]];
+            if(ws_searchActions.hasOwnProperty(input)){
+                ws_searchActions[input]();
             } else {
                 console.error('The command \'' + command[i] + '\' was not recognized');
             }
         }
     },
     componentWillUnmount: function(){
+        // when the component unmounts, destroy the select2 instance
         $('#search').select2('destroy');
     },
     handleFocus: function(){
+        // when the search bar is focused, turn off keyboard shortcuts for the
+        // rest of the worksheet. This is done in two parts: change the interactions store
+        // then trigger Worksheet to update its state
         ws_interactions.state.worksheetKeyboardShortcuts = false;
         ws_broker.fire('updateState');
     },
     handleBlur: function(){
+        // see handleFocus method
         ws_interactions.state.worksheetKeyboardShortcuts = true;
         ws_broker.fire('updateState');
     },
