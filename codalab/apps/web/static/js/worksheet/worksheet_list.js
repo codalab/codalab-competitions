3b19cd008b32c6020fc0673ad2e2143389bf773c
Adam Lord <adam.lord@gmail.com>
2014-09-12 17:05:00 -0700

    Worksheet List refactor: proof of concept
    - Consolidate all components under one Worksheets parent component
    - parent component handles which child component is 'active'
    - parent component listens for keyboard shortcuts and handles them, or dispatches the event to the active child component, or does nothing if the active component doesn't care
    - parent component keeps track of text to filter by, list component does the filtering at render time

--- codalab/apps/web/static/js/worksheet/worksheet_list.js
+++ codalab/apps/web/static/js/worksheet/worksheet_list.js
@@ -5,8 +5,62 @@ var keyMap = {
     40: "down",
     74: "j",
     75: "k",
-    88: "x"
-}
+    88: "x",
+    220: "bslash"
+};
+
+var Worksheets = React.createClass({
+    getInitialState: function(){
+        return {
+            activeComponent:"list",
+            filter: "",
+        }
+    },
+    handleFocus: function(event){
+        if(event.type=="focus"){
+            this.setState({activeComponent:"search"});
+        }else if(event.type=="blur"){
+            this.setState({activeComponent:"list"});
+        }
+    },
+    bindEvents: function(){
+        window.addEventListener('keydown', this.handleKeyboardShortcuts);
+    },
+    unbindEvents: function(){
+        window.removeEventListener('keydown', this.handleKeyboardShortcuts);
+    },
+    setFilter: function(event){
+        this.setState({filter:event.target.value})
+    },
+    componentDidMount: function(){
+        this.bindEvents();
+    },
+    componentWillUnmount: function(){
+        this.unbindEvents();
+    },
+    handleKeyboardShortcuts: function(event){
+        if(keyMap[event.keyCode] == 'bslash'){
+            event.preventDefault();
+            this.refs.search.getDOMNode().focus();
+        }
+        // dispatch the keycode to the active component
+        var activeComponent = this.refs[this.state.activeComponent];
+        if(activeComponent.hasOwnProperty('handleKeyboardShortcuts')){
+            activeComponent.handleKeyboardShortcuts(event);
+        }else {
+            return true;
+        }
+    },
+    render: function(){
+        return(
+            <div>
+                <WorksheetSearch setFilter={this.setFilter} handleFocus={this.handleFocus} ref={"search"} active={this.state.activeComponent=='search'} />
+                <WorksheetList handleFocus={this.handleFocus} ref={"list"} active={this.state.activeComponent=='list'} filter={this.state.filter} />
+            </div>
+        )
+    }
+});
+
 var WorksheetList = React.createClass({
     getInitialState: function(){
         return {
@@ -14,12 +68,6 @@ var WorksheetList = React.createClass({
             focusIndex: 0,
             myWorksheets: false
         };
-    },
-    bindEvents: function(){
-        window.addEventListener('keydown', this.handleKeyboardShortcuts);
-    },
-    unbindEvents: function(){
-        window.removeEventListener('keydown', this.handleKeyboardShortcuts);
     },
     componentDidMount: function() {
         $.ajax({
@@ -38,16 +86,12 @@ var WorksheetList = React.createClass({
                 $("#worksheet_list").html("Couldn\'t retrieve worksheets - Try refreshing the page");
             }.bind(this)
         });
-        this.bindEvents();
     },
     componentDidUpdate: function(){
         var itemNode = this.refs['ws' + this.state.focusIndex].getDOMNode();
         if(itemNode.offsetTop > window.innerHeight / 2){
             window.scrollTo(0, itemNode.offsetTop - (window.innerHeight / 2));
         }
-    },
-    componentWillUnmount: function(){
-        this.unbindEvents();
     },
     goToFocusedWorksheet: function(){
         window.location.href += this.state.worksheets[this.state.focusIndex].uuid;
@@ -56,28 +100,36 @@ var WorksheetList = React.createClass({
         this.setState({myWorksheets: !this.state.myWorksheets});
     },
     handleKeyboardShortcuts: function(event) {
-        if(ws_interactions.state.worksheetKeyboardShortcuts){
-            var key = keyMap[event.keyCode];
-            if(typeof key !== 'undefined'){
-                event.preventDefault();
-                if(key == 'k' || key == 'up'){
-                    this.setState({focusIndex: Math.max(this.state.focusIndex - 1, 0)});
-                }else if (key == 'j' || key == 'down'){
-                    this.setState({focusIndex: Math.min(this.state.focusIndex + 1, this.state.worksheets.length - 1)});
-                }else if (key == 'x' || key == 'enter'){
-                    this.goToFocusedWorksheet();
-                }else {
-                    return false;
-                }
+        var key = keyMap[event.keyCode];
+        if(typeof key !== 'undefined'){
+            event.preventDefault();
+            if(key == 'k' || key == 'up'){
+                this.setState({focusIndex: Math.max(this.state.focusIndex - 1, 0)});
+            }else if (key == 'j' || key == 'down'){
+                this.setState({focusIndex: Math.min(this.state.focusIndex + 1, this.state.worksheets.length - 1)});
+            }else if (key == 'x' || key == 'enter'){
+                this.goToFocusedWorksheet();
+            }else {
+                return false;
             }
         }
     },
-    render: function() {
-        var focusIndex = this.state.focusIndex;
+    filterWorksheets:function(filter){
         var worksheets = this.state.worksheets;
         if(this.state.myWorksheets){
-            worksheets = worksheets.filter(function(w){ return w.owner_id === user_id; });
+            worksheets = worksheets.filter(function(ws){ return ws.owner_id === user_id; });
         }
+        if(this.props.filter.length){
+            console.log('filtering by: ' + filter);
+            worksheets = worksheets.filter(function(ws){ 
+                return (ws.name.indexOf(filter) > -1);
+            });
+        }
+        return worksheets;
+    },
+    render: function() {
+        var worksheets = this.filterWorksheets(this.props.filter);
+        var focusIndex = worksheets.length > 1 ? this.state.focusIndex : 0;
         var worksheetList = worksheets.map(function(worksheet, index){
             var wsID = 'ws' + index;
             var focused = focusIndex === index;
@@ -119,7 +171,15 @@ var Worksheet = React.createClass({
             </div>
         );
     }
-})
+});
 
-React.renderComponent(<WorksheetList />, document.getElementById('worksheets'));
+var WorksheetSearch = React.createClass({
+    render: function(){
+        return (      
+            <input id="search" className="ws-search" type="text" placeholder="Search worksheets" onChange={this.props.setFilter} onFocus={this.props.handleFocus} onBlur={this.props.handleFocus}/>
+        )
+    }
+});
+
+React.renderComponent(<Worksheets />, document.getElementById('container'));
 
