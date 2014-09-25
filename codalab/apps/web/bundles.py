5ffbe0daebfcbd11adc30d756662217b3e74bdf0
Francis <francis@tivix.com>
2014-09-25 15:08:12 -0700

    Can able to save metadata also permissions #561

--- codalab/apps/web/bundles.py
+++ codalab/apps/web/bundles.py
@@ -82,7 +82,7 @@ if len(settings.BUNDLE_SERVICE_URL) > 0:
                 return 404
             return 500
 
-        def update_bundle_metadata(self, uuid, metadata):
+        def update_bundle_metadata(self, uuid, new_metadata):
             self.client.update_bundle_metadata(uuid, new_metadata)
             return
 
