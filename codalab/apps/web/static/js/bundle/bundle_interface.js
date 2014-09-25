bbb087f3208d32b82fa5f93cf031a8176431744a
Eric Carmichael <eric@ckcollab.com>
2014-09-24 23:26:00 -0700

    can browse folders now... can't go back yet

--- codalab/apps/web/static/js/bundle/bundle_interface.js
+++ codalab/apps/web/static/js/bundle/bundle_interface.js
@@ -12,7 +12,9 @@ var Bundle = React.createClass({
             "bundle_type": "",
             "metadata": {},
             "files": {},
-            "showFileBrowser": false
+            "showFileBrowser": false,
+            "fileBrowserData": "",
+            "currentWorkingDirectory": ""
         };
     },
     componentWillMount: function() {  // once on the page lets get the bundle info
@@ -40,14 +42,41 @@ var Bundle = React.createClass({
         });
     },
     toggleFileBrowser: function() {
+        this.setState({"showFileBrowser": !this.state.showFileBrowser});
+
+        // it's still false, but is true
+        if(!this.state.showFileBrowser) {
+            this.updateFileBrowser();
+        }
+    },
+    // File browser is updated based on location.hash!
+    updateFileBrowser: function(specific_folder_path) {
+        var folder_path = specific_folder_path || '';
+
+        if(folder_path == '') {
+            folder_path = location.hash.replace('#', '');
+        }
+
+        if(this.state.currentWorkingDirectory != '') {
+            folder_path = this.state.currentWorkingDirectory + "/" + folder_path;
+            this.setState({"currentWorkingDirectory": folder_path});
+        } else {
+            this.setState({"currentWorkingDirectory": folder_path});
+        }
+
+        location.hash = folder_path;
+
+        console.log("fp: " + folder_path);
+        console.log("cwd: " + this.state.currentWorkingDirectory);
+
         $.ajax({
             type: "GET",
             //  /api/bundles/0x706<...>d5b66e
-            url: document.location.pathname.replace('/bundles/', '/api/bundles/content/') + '/', //extra slash at end means root dir
+            url: document.location.pathname.replace('/bundles/', '/api/bundles/content/') + folder_path + '/', //extra slash at end means root dir
             dataType: 'json',
             cache: false,
             success: function(data) {
-                console.log(data);
+                this.setState({"fileBrowserData": data});
             }.bind(this),
             error: function(xhr, status, err) {
                 if (xhr.status != 404) {
@@ -58,7 +87,6 @@ var Bundle = React.createClass({
                 $('.bundle-file-view-container').hide();
             }.bind(this)
         });
-        this.setState({"showFileBrowser": !this.state.showFileBrowser});
     },
     render: function() {
         var metadata = this.state.metadata;
@@ -69,7 +97,7 @@ var Bundle = React.createClass({
         }
         bundle_download_url = "/bundles/" + this.state.uuid + "/download";
 
-        var fileBrowser = <FileBrowser />;
+        var fileBrowser = <FileBrowser fileBrowserData={this.state.fileBrowserData} updateFileBrowser={this.updateFileBrowser} />;
 
         return (
             <div className="row">
@@ -137,42 +165,80 @@ var BundleAttr = React.createClass({
 var FileBrowser = React.createClass({
     render: function() {
         var items = [];
+        var item;
 
-        items.push(<FileBrowserItem key='test1.py' type="file" />);
-        items.push(<FileBrowserItem key='test2.py' type="directory" />);
-
-        return (
-            <div className="row">
-                <div className="large-12 columns">
-                    <div className="bundle-tile">
-                        <h4><b>File Browser</b></h4>
-                        <table>
-                            <thead>
-                                <th>File name</th>
-                            </thead>
-                            <tbody>
-                                {items}
-                            </tbody>
-                        </table>
+        if(!this.props.fileBrowserData.contents) {
+            return (
+                <div className="row">
+                    <div className="large-12 columns">
+                        <div className="bundle-tile">
+                            <h4>
+                                <b>File Browser</b>
+                            </h4>
+                            <b>No files found</b>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+                );
+        } else {
+            // One loop for folders so they are on the top of the list
+            for (var i = 0; i < this.props.fileBrowserData.contents.length; i++) {
+                item = this.props.fileBrowserData.contents[i];
+                if (item.type == 'directory') {
+                    items.push(<FileBrowserItem key={item.name} type={item.type} updateFileBrowser={this.props.updateFileBrowser} />);
+                }
+            }
+
+            // Next loop for files
+            for (var i = 0; i < this.props.fileBrowserData.contents.length; i++) {
+                item = this.props.fileBrowserData.contents[i];
+                if (item.type != 'directory') {
+                    items.push(<FileBrowserItem key={item.name} type={item.type} updateFileBrowser={this.props.updateFileBrowser} />);
+                }
+            }
+
+            return (
+                <div className="row">
+                    <div className="large-12 columns">
+                        <div className="bundle-tile">
+                            <h4>
+                                <b>File Browser</b>
+                            </h4>
+                            <table>
+                                <thead>
+                                    <th>File name</th>
+                                </thead>
+                                <tbody>
+                                    {items}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                );
+        }
     }
 });
 
 var FileBrowserItem = React.createClass({
+    linkClicked: function(evt) {
+        this.props.updateFileBrowser(this.props.key);
+    },
     render: function() {
         // Type can be 'file' or 'folder'
-        var icon = "directory icon";
+        var icon = "icon_folder";
+        var link = this.props.key;
         if(this.props.type == "file") {
-            icon = "my-file-class"
+            icon = "icon_document"
         }
 
         return (
             <tr>
                 <td>
-                    {icon} <a href="#">{this.props.key}</a> click this to view file OR if folder go there
+                    <div onClick={this.linkClicked}>
+                        <img src={"/static/img/" + icon + ".png"} alt="More" />&nbsp;
+                        <b>{this.props.key}</b>
+                    </div>
                 </td>
             </tr>
         );
