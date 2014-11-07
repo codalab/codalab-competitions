ca1f709019e595da2f3bcd0b345da470bf9794bf
Adam Lord <adam.lord@gmail.com>
2014-11-07 11:19:32 -0800

    #673 clean up and refactor
    - move non-select2 functionality into ws_actions
    - maintain command dictionary in a format that makes sense
    - add a method to transform that dictionary into something select2 easily understands
    - update executeCommands method to look up its function in the command dict
    - add comments
    - cosmetic fixes to select2 spinner and outline color

--- codalab/apps/web/static/js/search/ws_actions.js
+++ codalab/apps/web/static/js/search/ws_actions.js
@@ -15,6 +15,32 @@ function WorksheetActions() {
         alert('Make a call to the cli to add the bundle with UUID ' + bundleID +
             ' to this worksheet, which has the UUID ' + worksheetID);
     };
-
+    // Dictionary of terms that can be entered into the search bar and the names of
+    // functions they call.
+    instance.commands = {
+        'add': {
+            functionName: 'doAdd',
+            helpText: 'add - add a bundle to this worksheet name or uuid',
+            url: '/api/worksheets/'
+        },
+        'info': {
+            functionName: 'doInfo',
+            helpText: 'info - go to a bundle\'s info page',
+            url: '/api/worksheets/'
+        }
+    };
+    // the select2 autocomplete expects its data in a certain way, so we'll turn
+    // the relevant parts of the command dict. into an array it can work with
+    instance.getCommands = function(){
+        commandDict = this.commands;
+        commandList = [];
+        for(var key in commandDict){
+            commandList.push({
+                'id': key,
+                'text': commandDict[key].helpText
+            });
+        }
+        return commandList;
+    };
     return instance;
 }
