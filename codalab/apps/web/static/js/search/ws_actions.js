aecf032fe59f1b03858006c04c6c46eabcf86098
Adam Lord <adam.lord@gmail.com>
2014-11-07 16:10:00 -0800

    Clean up comments

--- codalab/apps/web/static/js/search/ws_actions.js
+++ codalab/apps/web/static/js/search/ws_actions.js
@@ -1,5 +1,4 @@
-// Singleton class to manage actions triggered by the
-// general search bar
+// Singleton class to manage actions triggered by the general search bar
 function WorksheetActions() {
     var instance;
     WorksheetActions = function WorksheetActions(){
@@ -9,19 +8,9 @@ function WorksheetActions() {
     instance = new WorksheetActions();
     instance.constructor = WorksheetActions();
 
-    instance.doAdd = function(params, command){
-        var bundleID = params[1];
-        var worksheetID = ws_obj.state.uuid;
-        alert('Make a call to the cli to add the bundle with UUID ' + bundleID +
-            ' to this worksheet, which has the UUID ' + worksheetID);
-    };
-    instance.doInfo = function(params, command){
-        // ATL replace with 'bundle' link
-        window.location = '/worksheets/' + params[1] + '/';
-    };
-    // Dictionary of terms that can be entered into the search bar and the names of
-    // functions they call.
     instance.commands = {
+        // Dictionary of terms that can be entered into the search bar
+        // and the names of functions they call.
         'add': {
             functionName: 'doAdd',
             helpText: 'add - add a bundle to this worksheet name or uuid',
@@ -33,9 +22,10 @@ function WorksheetActions() {
             url: '/api/worksheets/'
         }
     };
-    // the select2 autocomplete expects its data in a certain way, so we'll turn
-    // the relevant parts of the command dict. into an array it can work with
+
     instance.getCommands = function(){
+        // The select2 autocomplete expects its data in a certain way, so we'll turn
+        // relevant parts of the command dict into an array it can work with
         commandDict = this.commands;
         commandList = [];
         for(var key in commandDict){
@@ -46,5 +36,18 @@ function WorksheetActions() {
         }
         return commandList;
     };
+
+    instance.doAdd = function(params, command){
+        var bundleID = params[1];
+        var worksheetID = ws_obj.state.uuid;
+        alert('Make a call to the cli to add the bundle with UUID ' + bundleID +
+            ' to this worksheet, which has the UUID ' + worksheetID);
+    };
+
+    instance.doInfo = function(params, command){
+        // ATL replace with 'bundle' link
+        window.location = '/worksheets/' + params[1] + '/';
+    };
+
     return instance;
 }
