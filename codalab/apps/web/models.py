02726a2710c90264d57317051f46a8a0cadf8caa
Eric Carmichael <eric@ckcollab.com>
2015-02-23 14:43:21 -0800

    tests failing for delayed phase migrations, basic idea seems to be working though

--- codalab/apps/web/models.py
+++ codalab/apps/web/models.py
@@ -223,6 +223,12 @@ class Competition(models.Model):
         current_phase: The new phase object we are entering
         last_phase: The phase object to transfer submissions from
         '''
+        logger.info("Checking for submissions that may still be running")
+
+        if current_phase.submissions.filter(status__codename=CompetitionSubmissionStatus.RUNNING).exists():
+            logger.info('Some submissions still marked as processing for competition pk=%s' % self.pk)
+            return
+
         logger.info('Doing phase migration on competition pk=%s from phase: %s to phase: %s' %
                     (self.pk, last_phase.phasenumber, current_phase.phasenumber))
 
