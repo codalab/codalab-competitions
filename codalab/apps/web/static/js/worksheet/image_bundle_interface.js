9eff95945212afbe33e5729a26d8a45f33e51c13
Francis <francis@tivix.com>
2015-03-06 11:06:54 -0800

    Handle image properties if given
    fix for issue #876

--- codalab/apps/web/static/js/worksheet/image_bundle_interface.js
+++ codalab/apps/web/static/js/worksheet/image_bundle_interface.js
@@ -12,13 +12,21 @@ var ImageBundle = React.createClass({
     render: function() {
         var className = 'type-image' + (this.props.focused ? ' focused' : '');
         var checkbox = this.props.canEdit ? <input type="checkbox" className="ws-checkbox" onChange={this.handleCheck} checked={this.state.checked} disabled={!this.props.checkboxEnabled}/> : null;
-
         var src= "data:image/png;base64," + this.state.interpreted
+        var styles = {};
+        if(this.state.properties){
+            if(this.state.properties.hasOwnProperty('height')){
+                styles['height'] = this.state.properties.height + "px;"
+            }
+            if(this.state.properties.hasOwnProperty('width')){
+                styles['width'] = this.state.properties.width + "px;"
+            }
+        }
         return(
             <div className="ws-item" onClick={this.handleClick}>
                 {checkbox}
                 <div className={className} ref={this.props.item.state.ref}>
-                    <img src={src} />
+                    <img style={styles} src={src} />
                 </div>
             </div>
         );
