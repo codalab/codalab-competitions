54f13a98695ea1da2dd48bf4c2ade6a7f457e662
Eric Carmichael <eric@ckcollab.com>
2015-03-18 23:16:37 -0700

    adds new fields and some sanitizing

--- codalab/apps/web/views.py
+++ codalab/apps/web/views.py
@@ -410,6 +410,9 @@ class CompetitionSubmissionsPage(LoginRequiredMixin, TemplateView):
                         'description': submission.description,
                         'method_name': submission.method_name,
                         'method_description': submission.method_description,
+                        'project_url': submission.project_url,
+                        'publication_url': submission.publication_url,
+                        'bibtex': submission.bibtex,
                     }
                     submission_info_list.append(submission_info)
                 context['submission_info_list'] = submission_info_list
@@ -419,6 +422,9 @@ class CompetitionSubmissionsPage(LoginRequiredMixin, TemplateView):
             last_submission = models.CompetitionSubmission.objects.filter(participant=participant, phase=phase).latest('submitted_at')
             context['last_submission_method_name'] = last_submission.method_name
             context['last_submission_method_description'] = last_submission.method_description
+            context['last_submission_project_url'] = last_submission.project_url
+            context['last_submission_publication_url'] = last_submission.publication_url
+            context['last_submission_bibtex'] = last_submission.bibtex
         except ObjectDoesNotExist:
             pass
 
@@ -1197,8 +1203,11 @@ def download_leaderboard_results(request, competition_pk, phase_pk):
         for entry in leaderboard_entries:
             submission = entry.result
             username_or_team_name = submission.participant.user.username if not submission.participant.user.team_name else "Team %s " % submission.participant.user.team_name
-            file_name = "%s - %s.zip" % (username_or_team_name, submission.submission_number)
+            file_name = "%s - %s submission.zip" % (username_or_team_name, submission.submission_number)
             zip_file.writestr(file_name, submission.file.read())
+
+            output_file_name = "%s - %s output.zip" % (username_or_team_name, submission.submission_number)
+            zip_file.writestr(output_file_name, submission.output_file.read())
 
             profile_data_file_name = "%s - %s profile.txt" % (username_or_team_name, submission.submission_number)
             user_profile_data = {
@@ -1215,10 +1224,10 @@ def download_leaderboard_results(request, competition_pk, phase_pk):
             user_profile_data_string = '\n'.join(['%s: %s' % (k, v) for k, v in user_profile_data.items()])
             zip_file.writestr(profile_data_file_name, user_profile_data_string.encode('utf-8'))
 
-            submission_method_file_name = "%s - %s method.txt" % (username_or_team_name, submission.submission_number)
-            submission_method_file_string = "Method: %s\nDescription:\n%s" % (submission.method_name,
-                                                                              submission.method_description)
-            zip_file.writestr(submission_method_file_name, submission_method_file_string.encode('utf-8'))
+            metadata_fields = ['method_name', 'method_description', 'project_url', 'publication_url', 'bibtex']
+            submission_metadata_file_name = "%s - %s method.txt" % (username_or_team_name, submission.submission_number)
+            submission_metadata_file_string = "\n".join(["%s: %s" % (field, getattr(submission, field)) for field in metadata_fields])
+            zip_file.writestr(submission_metadata_file_name, submission_metadata_file_string.encode('utf-8'))
 
         zip_file.close()
 
