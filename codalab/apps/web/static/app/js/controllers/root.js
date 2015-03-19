393184ca1b66e62cc4eca09a9a76e341a6d9919b
Francis <francis@tivix.com>
2015-03-19 16:06:07 -0700

    removed font files and js for mathjax

--- codalab/apps/web/static/app/js/controllers/root.js
+++
@@ -1,9 +0,0 @@
-﻿'use strict';
-
-angular.module('codalab.controllers')
-    .controller('root', ['$scope', 'worksheetsApi', function($scope, worksheetsApi) {
-        worksheetsApi.info().then(function(info) {
-            $scope.config = info.config;
-            $scope.user = info.user;
-        });
-    }]);
