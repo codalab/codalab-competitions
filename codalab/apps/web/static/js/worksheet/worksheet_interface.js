d9c05a69ed3c627a5b509710216f8475caa7b597
Francis <francis@tivix.com>
2015-04-13 16:49:10 -0700

    add permissions to worksheet.
    #944

--- codalab/apps/web/static/js/worksheet/worksheet_interface.js
+++ codalab/apps/web/static/js/worksheet/worksheet_interface.js
@@ -327,6 +327,7 @@ var Worksheet = React.createClass({
                                     <div className="worksheet-name">
                                         <h1 className="worksheet-icon">{ws_obj.state.name}</h1>
                                         <div className="worksheet-author">{ws_obj.state.owner}</div>
+                                        <div className="worksheet-permission">Permission: {ws_obj.state.permission_str}</div>
                                     </div>
                                 </div>
                                 <div className="col-sm-6">
