d0bf525542616dc1c88ffc2bdcc12f09fb7c1d5f
Eric Carmichael <eric@ckcollab.com>
2015-04-13 10:06:58 -0700

    adds update_compute_worker fab command

--- codalab/codalabtools/deploy/fabfile.py
+++ codalab/codalabtools/deploy/fabfile.py
@@ -534,3 +534,10 @@ def get_database_dump():
 
     backup_dir = os.environ.get("CODALAB_MYSQL_BACKUP_DIR", "")
     get('/tmp/%s' % dump_file_name, backup_dir)
+
+@task
+def update_compute_worker():
+    cd('codalab')
+    run('git pull --rebase')
+    sudo('stop codalab-compute-worker')
+    sudo('start codalab-compute-worker')
