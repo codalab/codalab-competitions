f4cfae21397dad84f35d25e1bbe3aeee866c9753
Eric Carmichael <eric@ckcollab.com>
2015-04-17 12:37:02 -0700

    migrations failed on travis, trying lower case??

--- codalab/apps/forums/migrations/0002_add_forum_for_each_competition.py
+++ codalab/apps/forums/migrations/0002_add_forum_for_each_competition.py
@@ -11,7 +11,7 @@ class Migration(DataMigration):
         # Note: Don't use "from appname.models import ModelName".
         # Use orm.ModelName to refer to models in this application,
         # and orm['appname.ModelName'] for models in other applications.
-        competitions = orm['web.Competition'].objects.all()
+        competitions = orm['web.competition'].objects.all()
         for competition in competitions:
             orm.Forum.objects.get_or_create(competition=competition)
 
