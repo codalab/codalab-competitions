86b489fc87352589c30c432bd87405b684de2316
Eric Carmichael <eric@ckcollab.com>
2015-04-24 15:17:32 -0700

    missed a spot in the migration

--- codalab/apps/web/migrations/0040_auto__add_field_competition_allow_public_submissions.py
+++ codalab/apps/web/migrations/0040_auto__add_field_competition_allow_public_submissions.py
@@ -71,7 +71,7 @@ class Migration(SchemaMigration):
         u'web.competition': {
             'Meta': {'ordering': "['end_date']", 'object_name': 'Competition'},
             'admins': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'competition_admins'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['authenz.ClUser']"}),
-            'allow_public_submissions': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'allow_public_submissions': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'allow_teams': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'competitioninfo_creator'", 'to': u"orm['authenz.ClUser']"}),
             'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
