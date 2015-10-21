a9d1a8285cddf41fc9d4a29663c0f378e5b91903
Pujun Bhatnagar <pujun@gatech.edu>
2015-10-21 09:24:40 -0700

    update baseURL and Worksheet test

--- tests/test.py
+++ tests/test.py
@@ -8,7 +8,6 @@ from selenium.common.exceptions import NoSuchElementException
 class CodalabTestCase(unittest.TestCase):
 
     def setUp(self):
-        print "blah"
         self.browser = webdriver.Firefox()
         self.addCleanup(self.browser.quit)
         self.base_url = "http://localhost:8000/"
@@ -16,21 +15,32 @@ class CodalabTestCase(unittest.TestCase):
     def testBaseURL(self):
         self.browser.get(self.base_url)
         self.browser.maximize_window()
+
+        # check title
         self.assertIn('CodaLab - Home', self.browser.title)
-        for i, item in enumerate(['Worksheets', 'Competitions', 'Help']):
+
+        #check the list in the top right
+        for i, item in [(1, 'Worksheets'), (2, 'Competitions'), (3, 'Help')]:
             self.assertEqual(
                 item,
-                self.browser.find_element_by_xpath("html/body/nav/div/div[2]/ul/li[" + str(i+1) + "]/a").get_attribute('innerHTML')
+                self.browser.find_element_by_xpath("html/body/nav/div/div[2]/ul/li[" + str(i) + "]/a").get_attribute('innerHTML')
             )
-        for i, item in enumerate(['Create an Experiment!', 'Join a challenge!']):
+
+        for i, item in [(1, 'Worksheets'), (2, 'Competitions')]:
             self.assertEqual(
                 item,
-                self.browser.find_element_by_xpath("html/body/div[2]/div/div/div[" + str(i+1) + "]/div/div/a").get_attribute('innerHTML')
+                self.browser.find_element_by_xpath("html/body/div[2]/div/div/div[" + str(i) + "]/div/div/a").get_attribute('innerHTML')
             )
-        
+
         assert "Run reproducible experiments and created executable papers using worksheets" in self.browser.page_source
-        assert "Enter the existing competition to solve challenging data problems, or host your own." in self.browser.page_source
+        assert "Enter an existing competition to solve challenging data problems, or host your own." in self.browser.page_source
     
+        for i, item in ([(3, 'Example Worksheets'), (4, 'Latest Competitions')]):
+            self.assertEqual(
+                item,
+                self.browser.find_element_by_xpath("html/body/div[2]/div/div/div[" + str(i) + "]/h2").get_attribute('innerHTML')
+            )
+
     def testWorksheet(self):
         self.browser.get(self.base_url)
         self.browser.find_element_by_xpath("html/body/nav/div/div[2]/ul/li[1]/a").click()
