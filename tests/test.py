bafb61a515ea1abb08d6e4e85f8e3b8d671fbae9
Pujun Bhatnagar <pujun@gatech.edu>
2015-11-05 14:53:02 -0800

    remove test div dependancy

--- tests/test.py
+++ tests/test.py
@@ -31,38 +31,13 @@ class CodalabTestCase(unittest.TestCase):
         self.browser.get(self.base_url)
         self.browser.maximize_window()
 
+        # Make sure that there is no error displayed on the page
+        self.assertNotIn('ERROR', self.browser.page_source)
         # check title
         self.assertIn('CodaLab - Home', self.browser.title)
 
-        #check the list in the top right
-        for i, item in [(1, 'Worksheets'), (2, 'Competitions'), (3, 'Help')]:
-            self.assertEqual(
-                item,
-                self.browser.find_element_by_xpath("html/body/nav/div/div[2]/ul/li[" + str(i) + "]/a").get_attribute('innerHTML')
-            )
-
-        for i, item in [(1, 'Worksheets'), (2, 'Competitions')]:
-            self.assertEqual(
-                item,
-                self.browser.find_element_by_xpath("html/body/div[2]/div/div/div[" + str(i) + "]/div/div/a").get_attribute('innerHTML')
-            )
-
-        assert "Run reproducible experiments and created executable papers using worksheets" in self.browser.page_source
-        assert "Enter an existing competition to solve challenging data problems, or host your own." in self.browser.page_source
-
-        for i, item in ([(3, 'Example Worksheets'), (4, 'Latest Competitions')]):
-            self.assertEqual(
-                item,
-                self.browser.find_element_by_xpath("html/body/div[2]/div/div/div[" + str(i) + "]/h2").get_attribute('innerHTML')
-            )
-
-    def testWorksheet(self):
-        self.browser.get(self.base_url)
-        self.browser.maximize_window()
-        self.browser.find_element_by_xpath("html/body/nav/div/div[2]/ul/li[1]/a").click()
-        self.assertEqual('Worksheets',
-            self.browser.find_element_by_xpath("html/body/div[1]/div/h1").get_attribute('innerHTML')
-        )
+        for item in ['Worksheets', 'Competitions', 'Help', 'Example Worksheets', 'Latest Competitions']:
+            self.assertIn(item, self.browser.page_source)
 
     def testWorksheetContent(self):
         cl = 'cl'
@@ -88,10 +63,13 @@ class CodalabTestCase(unittest.TestCase):
         self.browser.implicitly_wait(4)
         self.browser.get(self.browser.find_element_by_link_text(temp_worksheet_name).get_attribute('href'))
 
-        # we need to wait for a sec to make sure that the AJAX calls have been completed.
+        # wait for a sec to make sure that the AJAX calls have been completed.
         # TODO: find a better and more selenium way to do it.
         import time
         time.sleep(1)
+
+        # Make sure that there is no error displayed on the page
+        self.assertNotIn('ERROR', self.browser.page_source)
 
         # check worksheet layout
         self.worksheetLayoutCheckHelper(temp_worksheet_name, temp_worksheet_uuid, temp_worksheet_title)
@@ -105,100 +83,24 @@ class CodalabTestCase(unittest.TestCase):
         # delete the temp-worksheet
         self.run_command([cl, 'wrm', '--force', temp_worksheet_name])
 
+        self.run_command(['rm', temp_file_name])
+
     def worksheetContentCheckHelper(self, temp_file_name, temp_worksheet_uuid, temp_worksheet_paragraph_content):
         # test paragraph content inserted at top of temp_worksheet
-        self.assertEqual(temp_worksheet_paragraph_content,
-            self.browser.find_element_by_xpath('html/body/div[3]/div/div[2]/div[2]/div[1]/div/div[2]/div[1]/div/p').get_attribute('innerHTML')
-            )
+        self.assertIn(temp_worksheet_paragraph_content, self.browser.page_source)
         # check table in worksheet
-        table_content = [
-            ('uuid', None),
-            ('name', temp_file_name),
-            ('description', None),
-            ('bundle_type', 'dataset'),
-            ('created', None),
-            ('dependencies', None),
-            ('command', 'MISSING'),
-            ('data_size', '0'),
-            ('state', 'ready')
-        ]
-        for i, (heading, value) in enumerate(table_content):
-            self.assertEqual(heading,
-                self.browser.find_element_by_xpath("html/body/div[3]/div/div[2]/div[2]/div[1]/div/div[2]/div/div/table/thead/tr/th[" + str(i+1) + "]").get_attribute('innerHTML')
-            )
-            if value is not None:
-                self.assertEqual(value,
-                    self.browser.find_element_by_xpath("html/body/div[3]/div/div[2]/div[2]/div[1]/div/div[2]//div/div/table/tbody/tr/td[" + str(i+1) + "]").get_attribute('innerHTML')
-                )
+        table_content = ['uuid','name', temp_file_name, 'description', 'bundle_type', 'dataset', 'created', 'dependencies', 'command', 'MISSING', 'data_size', '0','state', 'ready']
+        for item in table_content:
+            self.assertIn(item, self.browser.page_source)
 
     def worksheetLayoutCheckHelper(self, temp_worksheet_name, temp_worksheet_uuid, temp_worksheet_title):
         # check worksheet information
-        worksheetInfoContent = [
-            ('name:', temp_worksheet_name),
-            ('uuid:', temp_worksheet_uuid),
-            ('owner:', None),
-            ('permissions:', None),
-        ]
-
-        for i, (k, v) in enumerate(worksheetInfoContent):
-            self.assertEqual(k,
-                self.browser.find_element_by_xpath("html/body/div[3]/div/div[2]/div[2]/div[1]/div/div[1]/div/div[1]/div/div[" + str(i+1) + "]/b").get_attribute('innerHTML')
-            )
-            if v is not None:
-                self.assertEqual(v,
-                    self.browser.find_element_by_xpath("html/body/div[3]/div/div[2]/div[2]/div[1]/div/div[1]/div/div[1]/div/div[" + str(i+1) + "]/span[2]").get_attribute('innerHTML')
-                )
-
-
-        # check keyboard-shortcuts
-        self.assertEqual(' Keyboard Shortcuts',
-            self.browser.find_element_by_xpath('html/body/div[3]/div/div[2]/div[2]/div[1]/div/div[1]/div/div[2]/div/a/span').get_attribute('innerHTML')
-        )
-
-        # check viewing options
-        options = ['View', 'Edit source']
-        for i, option in enumerate(options):
-            self.assertEqual(option,
-                self.browser.find_element_by_xpath('html/body/div[3]/div/div[2]/div[2]/div[1]/div/div[1]/div/div[2]/div/div/div/button[' + str(i+1) + ']').get_attribute('innerHTML')
-            )
-
-        # check worksheet title
-        self.assertEqual(temp_worksheet_title,
-            self.browser.find_element_by_xpath('html/body/div[3]/div/div[2]/div[2]/div[1]/div/div[1]/div/h4').get_attribute('innerHTML')
-        )
-
+        for item in ['name:', 'uuid:', 'owner:', 'permissions:', "Keyboard Shortcuts", 'view', 'source', temp_worksheet_name, temp_worksheet_uuid, temp_worksheet_title]:
+            self.assertIn(item, self.browser.page_source)
 
     def worksheetSidePanelCheckHelper(self, temp_worksheet_name, temp_worksheet_uuid, temp_file_name):
-        sidepandelContent = [
-            ('name', temp_worksheet_name),
-            ('uuid', temp_worksheet_uuid),
-            ('owner', None),
-            ('permissions', None),
-        ]
-
-        for i, (k, v) in enumerate(sidepandelContent):
-            self.assertEqual(k,
-                self.browser.find_element_by_xpath("html/body/div[3]/div/div[2]/div[1]/div/table/tbody/tr[" + str(i+1) + "]/th").get_attribute('innerHTML')
-            )
-            if v is not None:
-                self.assertEqual(v,
-                    self.browser.find_element_by_xpath("html/body/div[3]/div/div[2]/div[1]/div/table/tbody/tr[" + str(i+1) + "]/td").get_attribute('innerHTML')
-                )
-
-        # check table headings
-        table_content = [
-            ('type', 'dataset'),
-            ('name', None),
-        ]
-
-        for i, (heading, content) in enumerate(table_content):
-            self.assertEqual(heading,
-                self.browser.find_element_by_xpath("html/body/div[3]/div/div[2]/div[1]/div/div/table/thead/tr/th[" + str(i+1) + "]").get_attribute('innerHTML')
-            )
-            if content is not None:
-                self.assertEqual(content,
-                    self.browser.find_element_by_xpath("html/body/div[3]/div/div[2]/div[1]/div/div/table/tbody/tr/td[" + str(i+1) + "]").get_attribute('innerHTML')
-                )
+        for item in ['name', 'uuid', 'owner', 'permissions', 'type', temp_worksheet_name, temp_worksheet_uuid]:
+            self.assertIn(item, self.browser.find_element_by_class_name('ws-panel').text)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
