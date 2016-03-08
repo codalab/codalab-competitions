badc48f144df7a7796d26b3962d8bee5ce7dc619
Flavio Zhingri <fzhingri24@gmail.com>
2016-03-08 18:13:44 -0500

    refactor: Cleaning up code

--- codalab/apps/api/routers.py
+++ codalab/apps/api/routers.py
@@ -1,7 +1,7 @@
 from rest_framework import routers
 
-from django.conf.urls import patterns, url
-from django.conf import settings
+from django.conf.urls import url
+
 
 from . import views
 
@@ -16,7 +16,6 @@ router.register(r'defaultcontent', views.DefaultContentViewSet)
 urlpatterns = router.urls
 
 urlpatterns += (
-    ### Competitions
 
     url(r'^competition/create$', views.CompetitionCreationApi.as_view(), name='api_competition_creation'),
     url(r'^competition/create/sas$', views.CompetitionCreationSasApi.as_view(), name='api_competition_creation_sas'),
@@ -37,22 +36,4 @@ urlpatterns += (
     url(r'^competition/(?P<competition_id>\d+)/pages/(?P<pk>\d+)$', views.competition_page, name='api_competition_page'),
     url(r'^competition/(?P<competition_id>\d+)/pages/$', views.competition_page_list, name='api_competition_page_list'),
     url(r'^competition/(?P<pk>\d+)/pages/(?P<entity_label>\w[\w\d\-\_]+)/$', views.competition_page_list, name='api_competition_page_list'),
-
-    ### Worksheets
-    url(r'^worksheets/$', views.WorksheetsListApi.as_view(), name='api_worksheets'),
-    url(r'^worksheets/add/$', views.WorksheetsAddApi.as_view(), name='api_worksheets_add'),
-    url(r'^worksheets/delete/$', views.WorksheetsDeleteApi.as_view(), name='api_worksheets_delete'),
-    url(r'^worksheets/search/$', views.WorksheetsSearchApi.as_view(), name='api_worksheet_search'),
-    url(r'^worksheets/get_uuid/$', views.WorksheetsGetUUIDApi.as_view(), name='api_worksheet_get_uuid'),
-    url(r'^worksheets/bundle_list/$', views.WorksheetsGetBundleListApi.as_view(), name='api_worksheet_bundle_list'),
-    url(r'^worksheets/command/$', views.WorksheetsCommandApi.as_view(), name='api_worksheets_command'),
-    url(r'^worksheets/(?P<uuid>[A-Za-z0-9]+)/$', views.WorksheetContentApi.as_view(), name='api_worksheet_content'),
-
-    url(r'^bundles/content/(?P<uuid>[A-Za-z0-9]+)/$', views.BundleContentApi.as_view(), name='api_bundle_content'),
-    url(r'^bundles/content/(?P<uuid>[A-Za-z0-9]+)/(?P<path>.*)/$', views.BundleContentApi.as_view(), name='api_bundle_content'),
-    url(r'^bundles/filecontent/(?P<uuid>[A-Za-z0-9]+)/(?P<path>.*)$', views.BundleFileContentApi.as_view(), name='api_bundle_filecontent'),
-    url(r'^bundles/search/$', views.BundleSearchApi.as_view(), name='api_bundle_search'),
-    url(r'^bundles/get_uuid/$', views.BundleGetUUIDApi.as_view(), name='api_bundle_get_uuid'),
-    url(r'^bundles/upload/$', views.BundleUploadApi.as_view(), name='api_bundle_upload'),
-    url(r'^bundles/(?P<uuid>[A-Za-z0-9]+)/$', views.BundleInfoApi.as_view(), name='api_bundle_info'),
 )
