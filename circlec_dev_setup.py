da8546800ce1c142c64cec1538149cf1760ab836
Flavio Zhingri <fzhingri24@gmail.com>
2016-04-11 19:52:01 -0400

    fix: Another attemp to fix circleci

---
+++ circlec_dev_setup.py
@@ -0,0 +1,24 @@
+"""
+Provide a template to define local configuration settings. Make a copy of this
+file named 'local.py' and set appropriate values for the settings.
+"""
+from base import DevBase
+
+
+class Dev(DevBase):
+    '''
+    This is needed to run circleci, it should be copied to local.py
+    '''
+    ENABLE_COMPETITIONS = True
+
+    ENABLE_WORKSHEETS = False
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': '',
+            'USER': 'root',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
+    }
