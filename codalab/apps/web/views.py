5165e5f82ac89053ab7767dd2d46751746ab369d
Flavio Zhingri <fzhingri24@gmail.com>
2016-08-16 16:44:57 -0400

    fix: Banners duplication
    + Return only distinct competitions
    + closes #1443

--- codalab/apps/web/views.py
+++ codalab/apps/web/views.py
@@ -145,7 +145,7 @@ def my_index(request):
     except:
         denied = -1
 
-    my_competitions = models.Competition.objects.filter(Q(creator=request.user) | Q(admins__in=[request.user])).order_by('-pk').select_related('creator')
+    my_competitions = models.Competition.objects.filter(Q(creator=request.user) | Q(admins__in=[request.user])).order_by('-pk').select_related('creator').distinct()
     published_competitions = models.Competition.objects.filter(published=True).select_related('creator', 'participants')
     published_competitions = reversed(sorted(published_competitions, key=lambda c: c.get_start_date))
     context_dict = {
