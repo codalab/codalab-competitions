18488fc464b43b255bf8dc756f9089084a0478b8
Eric Carmichael <eric@ckcollab.com>
2017-02-14 22:23:44 -0800

    more configuration tweaks for celery

--- codalab/codalab/settings/base.py
+++ codalab/codalab/settings/base.py
@@ -301,11 +301,11 @@ class Base(Settings):
         }
     }
     # Celery
-    # BROKER_HEARTBEAT = None  # We're using TCP keep-alive instead
-    # BROKER_CONNECTION_TIMEOUT = 30  # May require a long timeout due to Linux DNS timeouts etc
-    CELERY_RESULT_BACKEND = None  # AMQP is not recommended as result backend as it creates thousands of queues
-    CELERY_SEND_EVENTS = False  # Will not create celeryev.* queues
-    CELERY_EVENT_QUEUE_EXPIRES = 60  # Will delete all celeryev. queues without consumers after 1 minute.
+    BROKER_HEARTBEAT = None  # We're using TCP keep-alive instead
+    BROKER_CONNECTION_TIMEOUT = 30  # May require a long timeout due to Linux DNS timeouts etc
+    # CELERY_RESULT_BACKEND = None  # AMQP is not recommended as result backend as it creates thousands of queues
+    # CELERY_SEND_EVENTS = False  # Will not create celeryev.* queues
+    # CELERY_EVENT_QUEUE_EXPIRES = 60  # Will delete all celeryev. queues without consumers after 1 minute.
     CELERY_ACCEPT_CONTENT = ['json']
     CELERY_TASK_SERIALIZER = 'json'
     CELERY_RESULT_SERIALIZER = 'json'
