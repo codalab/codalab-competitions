f485efb3286a4c1bc6fb850b6f032200349a19fc
Eric Carmichael <eric@ckcollab.com>
2017-02-18 15:34:25 -0800

    add -Ofair to workers

--- codalab/tmux.sh
+++ codalab/tmux.sh
@@ -2,13 +2,13 @@ tmux kill-session -t codalab_workers
 tmux new -d -s codalab_workers
 
 tmux send-keys -t codalab_workers "source ~/.virtualenvs/codalab-competitions/bin/activate" ENTER
-tmux send-keys -t codalab_workers "celery -A codalab worker -l info -Q site-worker -n site-worker -Ofast" ENTER
+tmux send-keys -t codalab_workers "celery -A codalab worker -l info -Q site-worker -n site-worker -Ofast -Ofair" ENTER
 
 tmux split-window -t codalab_workers
 
 tmux send-keys -t codalab_workers.1 "source ~/.virtualenvs/codalab-competitions/bin/activate" ENTER
 # Note we only want 1 compute worker running at once
-tmux send-keys -t codalab_workers "celery -A codalab worker -l info -Q compute-worker -n compute-worker --concurrency=1" ENTER
+tmux send-keys -t codalab_workers "celery -A codalab worker -l info -Q compute-worker -n compute-worker --concurrency=1 -Ofast -Ofair" ENTER
 
 tmux set-option -g set-titles on
 tmux set-option -g set-titles-string 'Codalab Workers Ctrl + b then d to detach'
