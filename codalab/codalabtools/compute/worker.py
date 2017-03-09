d71b0659d43768e22ec8a175569090f69f2ffcbf
Eric Carmichael <eric@ckcollab.com>
2017-03-09 11:48:01 -0800

    remove conditional, we want to demote when running submissions

--- codalab/codalabtools/compute/worker.py
+++ codalab/codalabtools/compute/worker.py
@@ -423,10 +423,9 @@ def get_run_func(config):
                 logger.info("Invoking program: %s", prog_cmd)
 
                 startTime = time.time()
-                exit_code = None
                 timed_out = False
 
-                if 'Darwin' not in platform.platform() and os.environ.get("DJANGO_ENVIRONMENT") is 'test':
+                if 'Darwin' not in platform.platform():
                     prog_cmd = prog_cmd.replace("python", join(run_dir, "/home/azureuser/anaconda/bin/python"))
                     # Run as separate user
                     evaluator_process = Popen(
