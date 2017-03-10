0f8ce13ccaeb2b0ba90c1d35ff68b02798da666a
Jared Cain <jared.cain77@gmail.com>
2017-03-09 23:36:31 -0800

    Add django log port to envs

--- run_django.sh
+++ run_django.sh
@@ -17,7 +17,7 @@ python manage.py syncdb --migrate
 # Insert initial data into the database
 python scripts/initialize.py
 
-touch /var/log/django.log
+touch $DJANGO_LOG
 
 # start development server on public ip interface, on port 8000
-gunicorn codalab.wsgi --bind django:$DJANGO_PORT --access-logfile /var/log/django.log
+gunicorn codalab.wsgi --bind django:$DJANGO_PORT --access-logfile $DJANGO_LOG
