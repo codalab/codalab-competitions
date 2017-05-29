f870dd18de0266498ffb4c86b0d2aa829fc88df9
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2017-05-29 01:31:02 +0000

    Teams edit image updates

---
+++ codalab/apps/teams/static/teams/teams.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('.remove-image .btn').click(function() {
+		var target = $('#image-clear_id');
+		target.prop('checked', true);
+		$('.image-removed').css('display', 'block');
+		$('.remove-image').css('display', 'none');
+		$('.logo-image').css('display', 'none');
+	});
+});
