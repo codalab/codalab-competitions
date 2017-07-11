db1d21fbcfc94858ebfc5312b02a17c3aeea895a
Eric Carmichael <eric@ckcollab.com>
2017-07-11 11:57:49 -0700

    adds scoring program docker image and default docker image to phases

--- codalab/apps/web/models.py
+++ codalab/apps/web/models.py
@@ -774,6 +774,9 @@ class CompetitionPhase(models.Model):
     scoring_program_organizer_dataset = models.ForeignKey('OrganizerDataSet', null=True, blank=True, related_name="scoring_program_organizer_dataset", verbose_name="Scoring Program", on_delete=models.SET_NULL)
     phase_never_ends = models.BooleanField(default=False)
 
+    scoring_program_docker_image = models.CharField(max_length=128, default='', blank=True)
+    default_docker_image = models.CharField(max_length=128, default='', blank=True)
+
     class Meta:
         ordering = ['phasenumber']
 
--- codalab/apps/web/tasks.py
+++ codalab/apps/web/tasks.py
@@ -199,19 +199,22 @@ def _prepare_compute_worker_run(job_id, submission, is_prediction):
         stdout = submission.prediction_stdout_file.name
         stderr = submission.prediction_stderr_file.name
         output = submission.prediction_output_file.name
+        docker_image = submission.docker_image or submission.phase.default_docker_image or \
+                       settings.DEFAULT_WORKER_DOCKER_IMAGE
     else:
         # Scoring, if we're not predicting
         bundle_url = submission.runfile.name
         stdout = submission.stdout_file.name
         stderr = submission.stderr_file.name
         output = submission.output_file.name
+        docker_image = submission.scoring_program_docker_image or settings.DEFAULT_WORKER_DOCKER_IMAGE
 
     data = {
         "id": job_id,
         "task_type": "run",
         "task_args": {
             "submission_id": submission.pk,
-            "docker_image": submission.docker_image or settings.DEFAULT_WORKER_DOCKER_IMAGE,
+            "docker_image": docker_image,
             "bundle_url": _make_url_sassy(bundle_url),
             "stdout_url": _make_url_sassy(stdout, permission='w'),
             "stderr_url": _make_url_sassy(stderr, permission='w'),
