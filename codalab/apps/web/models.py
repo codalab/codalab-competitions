6a6caa69b04a5740e7a0b26a5dd197eacf7ff136
Eric Carmichael <eric@ckcollab.com>
2017-08-01 15:10:16 -0700

    static assets uploaded on competition unpack

--- codalab/apps/web/models.py
+++ codalab/apps/web/models.py
@@ -20,6 +20,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.cache import cache
 from django.core.exceptions import PermissionDenied
 from django.core.files import File
+from django.core.files.base import ContentFile
 from django.core.files.storage import get_storage_class
 from django.core.urlresolvers import reverse
 from django.db import IntegrityError
@@ -1878,6 +1879,15 @@ class CompetitionDefBundle(models.Model):
                     # Associate the score definition with its leaderboard
                     sdg = SubmissionScoreDefGroup.objects.create(scoredef=sd, group=leaderboards[vals['leaderboard']['label']])
                 logger.debug("CompetitionDefBundle::unpack created scores (pk=%s)", self.pk)
+
+        # Find any static files and save them to storage, ignoring actual assets directory itself
+        assets = list(filter(lambda x: x.startswith('assets/') and x != "assets/", zf.namelist()))
+        asset_path = "competition_assets/{}/{}"
+        for asset in assets:
+            public_path = asset_path.format(comp.pk, os.path.basename(asset))
+            data = ContentFile(zf.open(asset).read())
+            PublicStorage.save(public_path, data)
+            logger.info("Created asset @ {}".format(public_path))
 
         # Add owner as participant so they can view the competition
         approved = ParticipantStatus.objects.get(codename=ParticipantStatus.APPROVED)
