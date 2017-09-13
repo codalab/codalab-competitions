b248b9755e5f9e87c66bca98137fc5c1f1a31aef
Eric Carmichael <eric@ckcollab.com>
2017-09-13 11:09:35 -0700

    only allow admins to user switch

--- codalab/apps/web/middleware.py
+++ codalab/apps/web/middleware.py
@@ -10,6 +10,7 @@ import tempfile
 import StringIO
 
 from django.conf import settings
+from userswitch.middleware import UserSwitchMiddleware
 
 from apps.customizer.models import Configuration
 
@@ -127,3 +128,17 @@ class SingleCompetitionMiddleware(object):
 
             if config.header_logo:
                 settings.CUSTOM_HEADER_LOGO = config.header_logo.url
+
+
+class CodalabUserSwitchMiddleware(UserSwitchMiddleware):
+    """Augments the UserSwitchMiddleware helper from django-debug-toolbar to only allow superusers to
+    use this feature."""
+
+    def process_request(self, request):
+        if request.user.is_authenticated and request.user.is_superuser:
+            return super(CodalabUserSwitchMiddleware, self).process_request(request)
+
+    def process_response(self, request, response):
+        if request.user.is_authenticated and request.user.is_superuser:
+            return super(CodalabUserSwitchMiddleware, self).process_response(request, response)
+        return response
