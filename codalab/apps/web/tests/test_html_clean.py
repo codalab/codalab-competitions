202c6e40901f79f895109fd9172c9c00ea24d034
Eric Carmichael <eric@ckcollab.com>
2017-11-13 10:45:55 -0800

    add test with newlines

--- codalab/apps/web/tests/test_html_clean.py
+++ codalab/apps/web/tests/test_html_clean.py
@@ -7,6 +7,9 @@ from apps.web.utils import clean_html_script
 class HTMLCleanTestCase(TestCase):
     def test_script_tags_get_removed_from_html_content(self):
         self.assertEquals(clean_html_script("<script></script>"), "")
+        self.assertEquals(clean_html_script("""<
+        script src=\"https://www.googleorsomething\"
+        ></script>"""), "")
         self.assertEquals(clean_html_script("<script src=\"https://www.googleorsomething\"></script>"), "")
         self.assertEquals(clean_html_script("<script src=\"https://www.googleorsomething\">There's some stuff in between here too </script>"), "")
         self.assertEquals(clean_html_script("<script src=\"https://www.googleorsomething\">There's some stuff in between here too </script>There should be some stuff here."), "There should be some stuff here.")
