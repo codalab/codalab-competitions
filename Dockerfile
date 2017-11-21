31504e08769f5accc7428d895441863aabb48d87
Eric Carmichael <eric@ckcollab.com>
2017-11-21 10:50:47 -0800

    add backup script and volumes to store backups (#2063)

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM python:2.7
 
-RUN apt-get update && apt-get install -y npm python-mysqldb netcat nodejs-legacy
+RUN apt-get update && apt-get install -y npm netcat nodejs-legacy
 
-RUN easy_install MySQL-python
 RUN pip install --upgrade pip  # make things faster, hopefully
 
 COPY codalab/requirements/common.txt requirements.txt
---
+++ scripts/pg_dump.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import time
+
+from subprocess import call
+
+
+dump_name = time.strftime("%Y-%m-%d_%H:%M:%S.dump")
+
+print("Making dump {}".format(dump_name))
+
+# Make dump
+call([
+    'docker',
+    'exec',
+    'postgres',
+    'bash',
+    '-c',
+    'PGPASSWORD=$DB_PASSWORD pg_dump -Fc -U $DB_USER $DB_NAME > /app/backups/{}'.format(
+        dump_name
+    )
+])
+
+# Push/destroy dump
+call([
+    'docker', 'exec', 'django', 'python', 'manage.py', 'upload_backup', '{}'.format(dump_name)
+])
