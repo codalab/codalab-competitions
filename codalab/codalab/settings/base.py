4ac3ce300e75cee6d1db08bf7a6de42a4853ed76
Eric Carmichael <eric@ckcollab.com>
2017-12-02 01:01:35 -0800

    add environment variable for allauth email verification flag

--- codalab/codalab/settings/base.py
+++ codalab/codalab/settings/base.py
@@ -260,7 +260,7 @@ class Base(Settings):
     ACCOUNT_AUTHENTICATION_METHOD='username_email'
     ACCOUNT_EMAIL_REQUIRED = True
     ACCOUNT_USERNAME_REQUIRED = True
-    ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+    ACCOUNT_EMAIL_VERIFICATION = os.environ.get('ACCOUNT_EMAIL_VERIFICATION', 'mandatory')
     ACCOUNT_SIGNUP_FORM_CLASS = 'apps.authenz.forms.CodalabSignupForm'
 
     # Django Analytical configuration
