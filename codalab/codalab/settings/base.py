a7388ead7c242a9f7c4178ec117449d55118fbb6
anbangleo <anbangleo@gmail.com>
2017-12-22 10:55:34 +0800

    configure successfully locally

--- codalab/codalab/settings/base.py
+++ codalab/codalab/settings/base.py
@@ -81,11 +81,11 @@ class Base(Settings):
     # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
     # although not all choices may be available on all operating systems.
     # In a Windows environment this must be set to your system time zone.
-    TIME_ZONE = 'UTC'
+    TIME_ZONE = 'Asia/Shanghai'
 
     # Language code for this installation. All choices can be found here:
     # http://www.i18nguy.com/unicode/language-identifiers.html
-    LANGUAGE_CODE = 'en-us'
+    LANGUAGE_CODE = 'zh-cn'
 
     SITE_ID = 1
     CODALAB_SITE_DOMAIN = os.environ.get('CODALAB_SITE_DOMAIN', 'localhost')
