c4f7486ff09b9ef367e54ef2bf7aa27bffb92e8b
Tyler <mr.tyler.thomas@gmail.com>
2018-01-04 12:01:01 -0800

    Clean HTML cast to unicode instead of string (#2152)
    * Clean HTML cast to unicode instead of string
    
    * Add tests

--- codalab/apps/web/tests/test_html_clean.py
+++ codalab/apps/web/tests/test_html_clean.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 from django.test import TestCase
 
 from apps.authenz.models import ClUser
@@ -8,9 +9,14 @@ class HTMLCleanTestCase(TestCase):
     def test_script_tags_get_removed_from_html_content(self):
         self.assertEquals(clean_html_script("<script></script>"), "")
         self.assertEquals(clean_html_script("<script src=\"https://www.googleorsomething\"></script>"), "")
-        self.assertEquals(clean_html_script("<script src=\"https://www.googleorsomething\">There's some stuff in between here too </script>"), "")
-        self.assertEquals(clean_html_script("<script src=\"https://www.googleorsomething\">There's some stuff in between here too </script>There should be some stuff here."), "There should be some stuff here.")
+        self.assertEquals(clean_html_script(
+            "<script src=\"https://www.googleorsomething\">There's some stuff in between here too </script>"), "")
+        self.assertEquals(clean_html_script(
+            "<script src=\"https://www.googleorsomething\">There's some stuff in between here too </script>There should be some stuff here."),
+                          "There should be some stuff here.")
         self.assertEquals(clean_html_script("<b>A strong word</b>"), "<b>A strong word</b>")
+        self.assertEquals(clean_html_script(u'<h1 class="char Zinh U030C" data-text="̌">̌</h1>'),
+                          u'<h1 class="char Zinh U030C" data-text="̌">̌</h1>')
         self.assertEquals(clean_html_script({
             'my_key': 1,
             'my_value': "twenty_seven",
