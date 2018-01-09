6ae716c3ba2a861efd08a212d088da6276248517
Eric Carmichael <eric@ckcollab.com>
2018-01-09 11:30:56 -0800

    handle chahub being down

--- codalab/apps/chahub/models.py
+++ codalab/apps/chahub/models.py
@@ -29,6 +29,9 @@ class ChaHubSaveMixin(models.Model):
     class Meta:
         abstract = True
 
+    # -------------------------------------------------------------------------
+    # METHODS TO OVERRIDE WHEN USING THIS MIXIN!
+    # -------------------------------------------------------------------------
     def get_chahub_endpoint(self):
         """Override this to return the endpoint URL for this resource
 
@@ -38,6 +41,17 @@ class ChaHubSaveMixin(models.Model):
         """
         raise NotImplementedError()
 
+    def get_chahub_data(self):
+        """Override this to return a dictionary with data to send to chahub
+
+        Example:
+            return {"name": self.name}
+        """
+        raise NotImplementedError()
+
+    # -------------------------------------------------------------------------
+    # Regular methods
+    # -------------------------------------------------------------------------
     def get_chahub_url(self):
         assert settings.CHAHUB_API_URL, "No ChaHub URL given, cannot send details to ChaHub"
         assert settings.CHAHUB_API_URL.endswith("/"), "ChaHub API url must end with a slash"
@@ -47,38 +61,31 @@ class ChaHubSaveMixin(models.Model):
 
         return "{}{}".format(settings.CHAHUB_API_URL, endpoint)
 
-    def get_chahub_data(self):
-        """Override this to return a dictionary with data to send to chahub
-
-        Example:
-            return {"name": self.name}
-        """
-        raise NotImplementedError()
-
     def send_to_chahub(self, data):
         """Sends data to chahub and returns the HTTP response"""
         url = self.get_chahub_url()
 
         logger.info("ChaHub :: Sending to ChaHub ({}) the following data: \n{}".format(url, data))
 
-        return requests.post(url, data, headers={
-            'Content-type': 'application/json',
-            'X-CHAHUB-API-KEY': settings.CHAHUB_API_KEY,
-        })
+        try:
+            return requests.post(url, data, headers={
+                'Content-type': 'application/json',
+                'X-CHAHUB-API-KEY': settings.CHAHUB_API_KEY,
+            })
+        except requests.ConnectionError:
+            return None
 
     def save(self, *args, **kwargs):
         if settings.CHAHUB_API_URL:
             data = json.dumps(self.get_chahub_data())
             data_hash = hashlib.md5(data).hexdigest()
 
-            logger.info("self.sent_to_chahub: '{}' self.chahub_data_hash: '{}'".format(self.sent_to_chahub, self.chahub_data_hash))
-
             if not self.sent_to_chahub or self.chahub_data_hash != data_hash:
                 resp = self.send_to_chahub(data)
 
                 logger.info("ChaHub :: Received response {} {}".format(resp.status_code, resp.content))
 
-                if resp.status_code in (200, 201):
+                if resp and resp.status_code in (200, 201):
                     self.sent_to_chahub = timezone.now()
                     self.chahub_data_hash = data_hash
 
