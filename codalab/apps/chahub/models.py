bdd85e3a40ed856c6d673d1e20cc4ac5104aca9d
Eric Carmichael <eric@ckcollab.com>
2018-02-28 14:14:48 -0800

    automatically retry sending ChaHub updates if they fail, retries are attempted every 10 minutes:

--- codalab/apps/chahub/models.py
+++ codalab/apps/chahub/models.py
@@ -17,14 +17,17 @@ class ChaHubSaveMixin(models.Model):
     To use:
     1) Override `get_chahub_endpoint()` to return the endpoint on ChaHub API for this model
     2) Override `get_chahub_data()` to return a dictionary to send to ChaHub
-    3) Data is sent on `save()` and `sent_to_chahub` timestamp is set
+    3) Data is sent on `save()` and `chahub_timestamp` timestamp is set
 
-    To update remove the `sent_to_chahub` timestamp and call `save()`"""
+    To update remove the `chahub_timestamp` timestamp and call `save()`"""
     # Timestamp set whenever a successful update happens
-    sent_to_chahub = models.DateTimeField(null=True, blank=True)
+    chahub_timestamp = models.DateTimeField(null=True, blank=True)
 
     # A hash of the last json information that was sent to avoid sending duplicate information
     chahub_data_hash = models.TextField(null=True, blank=True)
+
+    # If sending to chahub fails, we may need a retry. Signal that by setting this attribute to True
+    chahub_needs_retry = models.BooleanField(default=False)
 
     class Meta:
         abstract = True
@@ -56,6 +59,7 @@ class ChaHubSaveMixin(models.Model):
             return comp.is_published
         """
         raise NotImplementedError()
+
 
     # -------------------------------------------------------------------------
     # Regular methods
@@ -90,17 +94,18 @@ class ChaHubSaveMixin(models.Model):
                 data = json.dumps(self.get_chahub_data())
                 data_hash = hashlib.md5(data).hexdigest()
 
-                if not self.sent_to_chahub or self.chahub_data_hash != data_hash:
+                if not self.chahub_timestamp or self.chahub_data_hash != data_hash:
                     resp = self.send_to_chahub(data)
 
-                    logger.info("ChaHub :: Received response {} {}".format(resp.status_code, resp.content))
-
                     if resp and resp.status_code in (200, 201):
-                        self.sent_to_chahub = timezone.now()
+                        logger.info("ChaHub :: Received response {} {}".format(resp.status_code, resp.content))
+                        self.chahub_timestamp = timezone.now()
                         self.chahub_data_hash = data_hash
-                else:
-                    print("Not sent to Chahub")
+                        self.chahub_needs_retry = False
+                    else:
+                        logger.info("ChaHub :: No response received, connection error?")
+                        self.chahub_needs_retry = True
             else:
-                logger.info("Competition failed validation")
+                logger.info("ChaHub :: Model failed validation")
 
         super(ChaHubSaveMixin, self).save(*args, **kwargs)
