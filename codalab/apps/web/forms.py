6a03ca0f160188fc528b25e7cbc50509694f2f4c
BrendanMyers88 <myersb88@gmail.com>
2018-07-16 11:31:19 -0700

    Check for Webpage name duplication. Returns error if name is a duplicate.

--- codalab/apps/web/forms.py
+++ codalab/apps/web/forms.py
@@ -1,6 +1,7 @@
 import os
 
 from django import forms
+from django.core.exceptions import ValidationError
 from django.core.files.base import ContentFile
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import get_user_model
@@ -139,6 +140,24 @@ class PageForm(forms.ModelForm):
         widgets = { 'html' : TinyMCE(attrs={'rows' : 20, 'class' : 'competition-editor-page-html'},
                                      mce_attrs={"theme" : "advanced", "cleanup_on_startup" : True, "theme_advanced_toolbar_location" : "top", "gecko_spellcheck" : True}),
                     'DELETE' : forms.HiddenInput, 'container' : forms.HiddenInput}
+
+    def clean_label(self):
+        cleaned_data = super(PageForm, self).clean()
+        label = cleaned_data.get('label')
+        if label:
+            existing_website = models.Page.objects.filter(
+                competition=self.instance.competition,
+                label=label
+            ).exclude(pk=self.instance.pk)
+            if existing_website.exists():
+                raise forms.ValidationError(
+                    {
+                        'name': ['Invalid value for website name. A website already exists with that name.']
+                    },
+                    code='invalid'
+                )
+
+        return label
 
     def save(self, commit=True):
 
