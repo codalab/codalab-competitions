0edf39fbe51b54e8e6112001c81ecf199f8c6012
Eric Carmichael <eric@ckcollab.com>
2018-09-20 15:16:12 -0700

    fix tests, passing local

--- codalab/apps/common/tests/test_featured_competitions.py
+++ codalab/apps/common/tests/test_featured_competitions.py
@@ -24,54 +24,78 @@ class FeaturedCompetitionsTests(TestCase):
         '''
 
         self.user = User.objects.create(email="user1@tes.com", username="user1", password="pass")
-        self.user1 = User.objects.create(email="user@test.com", username="user", password="pass")
-        self.user2 = User.objects.create(email="user2@test.com", username="user2", password="pass")
-        self.user3 = User.objects.create(email="user3@test.com", username="user3", password="pass")
-        self.user4 = User.objects.create(email="user4@test.com", username="user4", password="pass")
-        self.user5 = User.objects.create(email="user5@test.com", username="user5", password="pass")
 
-        self.competition1 = Competition.objects.create(creator=self.user, modified_by=self.user, published=True)
-        self.competition2 = Competition.objects.create(creator=self.user, modified_by=self.user, published=True)
-        self.competition3 = Competition.objects.create(creator=self.user, modified_by=self.user, published=True)
-        self.competition4 = Competition.objects.create(creator=self.user, modified_by=self.user, published=True)
-        self.competition5 = Competition.objects.create(creator=self.user, modified_by=self.user, published=True)
+        self.competition1 = Competition.objects.create(title="competition1", creator=self.user, modified_by=self.user, published=True)
+        self.competition2 = Competition.objects.create(title="competition2", creator=self.user, modified_by=self.user, published=True)
+        self.competition3 = Competition.objects.create(title="competition3", creator=self.user, modified_by=self.user, published=True)
+        self.competition4 = Competition.objects.create(title="competition4", creator=self.user, modified_by=self.user, published=True)
+        self.competition5 = Competition.objects.create(title="competition5", creator=self.user, modified_by=self.user, published=True)
+        self.competition_old = Competition.objects.create(
+            title="competition_old",
+            creator=self.user,
+            modified_by=self.user,
+            published=True,
+            start_date=now() - datetime.timedelta(days=50)
+        )
+        self.competition_oldest = Competition.objects.create(
+            title="competition_oldest",
+            creator=self.user,
+            modified_by=self.user,
+            published=True,
+            start_date=now() - datetime.timedelta(days=500)
+        )
 
+        submission_finished = CompetitionSubmissionStatus.objects.create(name="finished", codename="finished")
+
+        # First submission, new
         self.participant1 = CompetitionParticipant.objects.create(
             user=self.user,
             competition=self.competition1,
             status=ParticipantStatus.objects.get_or_create(name='approved', codename=ParticipantStatus.APPROVED)[0]
         )
-
         self.phase1 = CompetitionPhase.objects.create(
             competition=self.competition1,
             phasenumber=1,
-            start_date=datetime.datetime.now() - datetime.timedelta(days=30),
+            start_date=datetime.datetime.now() - datetime.timedelta(days=29),  # less than 30 days
         )
-
-        submission_finished = CompetitionSubmissionStatus.objects.create(name="finished", codename="finished")
-
         self.submission1 = CompetitionSubmission.objects.create(
             participant=self.participant1,
             phase=self.phase1,
             status=submission_finished,
-            submitted_at=now() - datetime.timedelta(days=29),
-            description=u"Some description with unicode \u2020"
         )
+        self.submission1.submitted_at = now() - datetime.timedelta(days=5)  # less than 7 days old
+        self.submission1.save()
 
-    def test_get_featured_competitions_returns_competition_with_1_submission_past_week(self):
-        competitions = get_featured_competitions()
-        # Should only return #1 because it is the only one with a submission
+        # Second submission, old
+        self.participant2 = CompetitionParticipant.objects.create(
+            user=self.user,
+            competition=self.competition_old,
+            status=ParticipantStatus.objects.get_or_create(name='approved', codename=ParticipantStatus.APPROVED)[0]
+        )
+        self.phase2 = CompetitionPhase.objects.create(
+            competition=self.competition_old,
+            phasenumber=1,
+            start_date=datetime.datetime.now() - datetime.timedelta(days=35),  # over 30 days old
+        )
+        self.submission2 = CompetitionSubmission.objects.create(
+            participant=self.participant2,
+            phase=self.phase2,
+            status=submission_finished,
+        )
+        self.submission2.submitted_at = now() - datetime.timedelta(days=34)  # older than 30 days
+        self.submission2.save()
+
+    def test_get_featured_competitions_returns_newer_competitions(self):
+        competitions = get_featured_competitions(limit=1)
+        # Should always return the 1 new competition
         assert len(competitions) == 1
-        assert competitions[0].pk == self.competition1.pk
+        assert self.competition_old not in competitions
 
-    def test_get_featured_competitions_returns_competition_started_one_month_ago(self):
-        old_competition = Competition.objects.create(
-            creator=self.user,
-            modified_by=self.user,
-            published=True,
-            start_date=now() - datetime.timedelta(days=31)
-        )
-        competitions = get_featured_competitions()
-        # Should only have the 5 original competitions not the older competition
-        assert len(competitions) == 5
-        assert old_competition.pk not in [c.pk for c in competitions]
+        # Delete new competition, mark submission as newer, old competition should now appear in list
+        self.competition1.delete()
+        self.submission2.submitted_at = now()
+        self.submission2.save()
+        competitions = get_featured_competitions(limit=1)
+
+        assert len(competitions) == 1
+        assert self.competition_old in competitions
