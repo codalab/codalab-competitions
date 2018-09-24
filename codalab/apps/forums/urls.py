ffca839779e404d3c576c9f06cd56c986a626476
Eric Carmichael <eric@ckcollab.com>
2018-09-24 12:26:48 -0700

    forum thread pinning added

--- codalab/apps/forums/urls.py
+++ codalab/apps/forums/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 
 urlpatterns = patterns('',
+    url(r'^pin/(?P<thread_pk>\d+)/$', views.pin_thread, name='forum_thread_pin'),
+
     url(r'^(?P<forum_pk>\d+)/$', views.ForumDetailView.as_view(), name='forum_detail'),
     url(r'^(?P<forum_pk>\d+)/new_thread/$', views.CreateThreadView.as_view(), name='forum_new_thread'),
     url(r'^(?P<forum_pk>\d+)/(?P<thread_pk>\d+)/$', views.ThreadDetailView.as_view(), name='forum_thread_detail'),
