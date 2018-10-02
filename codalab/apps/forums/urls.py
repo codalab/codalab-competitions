61975654c517808d0cd2085368dc30968aa33a8b
Eric Carmichael <eric@ckcollab.com>
2018-10-02 16:54:26 -0700

    Forum improvements (#2420)
    * Adds delete for threads and posts, and post content is now included in notification emails
    
    * forum thread pinning added
    
    * flag -> pushpin
    
    * fix reference to admins
    
    * better notification emails
    
    * don't notify own user of their post
    
    * allows disabling of notifications for forums
    
    * add forum notifications to settings form fields
    
    * Update forums.css

--- codalab/apps/forums/urls.py
+++ codalab/apps/forums/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 
 urlpatterns = patterns('',
+    url(r'^pin/(?P<thread_pk>\d+)/$', views.pin_thread, name='forum_thread_pin'),
+
     url(r'^(?P<forum_pk>\d+)/$', views.ForumDetailView.as_view(), name='forum_detail'),
     url(r'^(?P<forum_pk>\d+)/new_thread/$', views.CreateThreadView.as_view(), name='forum_new_thread'),
     url(r'^(?P<forum_pk>\d+)/(?P<thread_pk>\d+)/$', views.ThreadDetailView.as_view(), name='forum_thread_detail'),
     url(r'^(?P<forum_pk>\d+)/(?P<thread_pk>\d+)/new_post/$', views.CreatePostView.as_view(), name='forum_new_post'),
+    url(r'^(?P<forum_pk>\d+)/(?P<thread_pk>\d+)/delete/$', views.DeleteThreadView.as_view(), name='forum_delete_thread'),
+    url(r'^(?P<forum_pk>\d+)/(?P<thread_pk>\d+)/delete/(?P<post_pk>\d+)/$', views.DeletePostView.as_view(), name='forum_delete_post'),
 )
