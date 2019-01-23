cd200970a6f3c26a01e82e534464ddfe0c685be3
Eric Carmichael <eric@ckcollab.com>
2019-01-23 11:21:34 -0800

    bump version when running worker, as well

--- codalab/fabfile.py
+++ codalab/fabfile.py
@@ -160,7 +160,7 @@ def compute_worker_run():
         "--name compute_worker "
         "--log-opt max-size=50m "
         "--log-opt max-file=3 -- "
-        "codalab/competitions-v1-compute-worker:1.1.4")
+        "codalab/competitions-v1-compute-worker:1.1.5")
 
 
 def compute_worker_status():
