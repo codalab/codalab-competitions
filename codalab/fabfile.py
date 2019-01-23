6868f9a7c1985842d28c029167b626a91862a1fa
Eric Carmichael <eric@ckcollab.com>
2019-01-23 11:21:02 -0800

    bump compute worker version

--- codalab/fabfile.py
+++ codalab/fabfile.py
@@ -93,7 +93,7 @@ def compute_worker_update():
         fab hosts:prod compute_worker_update
     """
     compute_worker_kill()
-    run('docker pull codalab/competitions-v1-compute-worker:1.1.4')
+    run('docker pull codalab/competitions-v1-compute-worker:1.1.5')
     run('docker pull {}'.format(django_settings.DOCKER_DEFAULT_WORKER_IMAGE))
     compute_worker_run()
 
