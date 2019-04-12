017bf2cb048178d91d66d487765bf4e7dfc80bfc
gibsonbailey <gibson.bailey@live.com>
2019-04-12 16:40:11 -0700

    Switch back to Null=True, blank=True to pass tests.

--- codalab/apps/health/migrations/0004_auto__add_field_healthsettings_congestion_threshold.py
+++ codalab/apps/health/migrations/0004_auto__add_field_healthsettings_congestion_threshold.py
@@ -10,7 +10,7 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         # Adding field 'HealthSettings.congestion_threshold'
         db.add_column(u'health_healthsettings', 'congestion_threshold',
-                      self.gf('django.db.models.fields.PositiveIntegerField')(default=100),
+                      self.gf('django.db.models.fields.PositiveIntegerField')(default=100, null=True, blank=True),
                       keep_default=False)
 
 
@@ -22,7 +22,7 @@ class Migration(SchemaMigration):
     models = {
         u'health.healthsettings': {
             'Meta': {'object_name': 'HealthSettings'},
-            'congestion_threshold': ('django.db.models.fields.PositiveIntegerField', [], {'default': '100'}),
+            'congestion_threshold': ('django.db.models.fields.PositiveIntegerField', [], {'default': '100', 'null': 'True', 'blank': 'True'}),
             'emails': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'threshold': ('django.db.models.fields.PositiveIntegerField', [], {'default': '25', 'null': 'True', 'blank': 'True'})
