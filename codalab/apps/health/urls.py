b1fd68b0e34da1871839f1ba2ba781a4000fb4ac
Tthomas63 <mr.tyler.thomas@gmail.com>
2019-04-12 15:23:16 -0700

    Cancel button on submissions page, simple health status, fix forums link

--- codalab/apps/health/urls.py
+++ codalab/apps/health/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^status', views.health, name='health_status'),
+    url(r'^simple_status', views.simple_health, name='health_status_simple'),
     url(r'^email_settings', views.email_settings, name='health_status_email_settings'),
     url(r'^check_thresholds', views.check_thresholds, name='health_status_check_thresholds')
 ]
