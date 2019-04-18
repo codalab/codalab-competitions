52cb652179bbb03b69f3c2797c3791031c9bf630
Tthomas63 <mr.tyler.thomas@gmail.com>
2019-04-18 09:00:19 -0700

    Add more info to simple status: Hostname (Not specific for predicition/scoring), filename, ID

--- codalab/apps/health/views.py
+++ codalab/apps/health/views.py
@@ -115,7 +115,7 @@ def simple_health(request):
     qs = qs.order_by('-submitted_at')
     qs = qs.select_related('phase__competition')
     # qs = qs.select_related('participant__user__username')
-    qs = qs.prefetch_related('phase', 'status')
+    qs = qs.prefetch_related('phase', 'status', 'participant')
     return render(request, "health/simple_health.html", {
         "submissions": qs[:250],
     })
