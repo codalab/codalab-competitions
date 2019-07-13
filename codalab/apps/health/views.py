94474ec978e18379025e07ea7f51b0d14042dcc4
Eric Carmichael <eric@ckcollab.com>
2019-07-12 17:32:31 -0700

    limit health page view

--- codalab/apps/health/views.py
+++ codalab/apps/health/views.py
@@ -119,7 +119,7 @@ def simple_health(request):
     qs = qs.select_related('participant__user__username')
     qs = qs.prefetch_related('phase', 'status')
     return render(request, "health/simple_health.html", {
-        "submissions": qs[:250],
+        "submissions": qs[:10],
     })
 
 
