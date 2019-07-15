f9107b94ba9991769f0982cb86062abec5dd43bb
BrendanMyers88 <myersb88@gmail.com>
2019-07-15 12:20:39 -0700

    Addes a check for mailchimp settings on script

--- codalab/apps/newsletter/scripts/send_emails_to_mailchimp.py
+++ codalab/apps/newsletter/scripts/send_emails_to_mailchimp.py
@@ -8,20 +8,21 @@ User = get_user_model()
 
 
 def run():
-    for user in User.objects.all():
-        data = {
-            "email_address": user.email,
-            "status": "subscribed",
-        }
+    if settings.MAILCHIMP_MEMBERS_ENDPOINT_ALL and settings.MAILCHIMP_API_KEY:
+        for user in User.objects.all():
+            data = {
+                "email_address": user.email,
+                "status": "subscribed",
+            }
 
-        requests.patch(
-            settings.MAILCHIMP_MEMBERS_ENDPOINT_ALL,
-            auth=("", settings.MAILCHIMP_API_KEY),
-            data=json.dumps(data)
-        )
+            requests.patch(
+                settings.MAILCHIMP_MEMBERS_ENDPOINT_ALL,
+                auth=("", settings.MAILCHIMP_API_KEY),
+                data=json.dumps(data)
+            )
 
-        requests.post(
-            settings.MAILCHIMP_MEMBERS_ENDPOINT_ALL,
-            auth=("", settings.MAILCHIMP_API_KEY),
-            data=json.dumps(data)
-        )
+            requests.post(
+                settings.MAILCHIMP_MEMBERS_ENDPOINT_ALL,
+                auth=("", settings.MAILCHIMP_API_KEY),
+                data=json.dumps(data)
+            )
