f577f7bbe4870cd800b907d189a49fe7b1961a2e
BrendanMyers88 <myersb88@gmail.com>
2019-07-24 16:13:06 -0700

    Now checks if codalab has a Mailchimp API Key. If it does, it will allow users to signup and unsubscribe from the mailing list. If not, it will 404 on the mailing list links and remove the ability to edit their opt_in status on account creation and in the user settings.

--- codalab/apps/authenz/forms.py
+++ codalab/apps/authenz/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from codalab import settings
 
 
 class CodalabSignupForm(forms.Form):
@@ -7,7 +8,8 @@ class CodalabSignupForm(forms.Form):
     """
     team_name = forms.CharField(max_length=64, required=False)
     contact_email = forms.EmailField(required=False)
-    newsletter_opt_in = forms.BooleanField(initial=False, required=False)
+    if settings.MAILCHIMP_API_KEY:
+        newsletter_opt_in = forms.BooleanField(initial=False, required=False)
     method_name = forms.CharField(max_length=20, required=False)
     method_description = forms.CharField(required=False)
     project_url = forms.URLField(required=False)
@@ -29,10 +31,13 @@ class CodalabSignupForm(forms.Form):
             'method_name': self.cleaned_data['method_name'],
             'method_description': self.cleaned_data['method_description'],
             'contact_email': self.cleaned_data['contact_email'],
-            'newsletter_opt_in': self.cleaned_data['newsletter_opt_in'],
             'project_url': self.cleaned_data['project_url'],
             'publication_url': self.cleaned_data['publication_url'],
             'bibtex': self.cleaned_data['bibtex'],
         })
-        new_user.save()
 
+        if settings.MAILCHIMP_API_KEY:
+            new_user.__dict__.update({
+                'newsletter_opt_in': self.cleaned_data['newsletter_opt_in'],
+            })
+        new_user.save()
--- codalab/apps/newsletter/views.py
+++ codalab/apps/newsletter/views.py
@@ -3,7 +3,7 @@ from django.contrib.sites.models import Site
 from django.shortcuts import render
 from django.core.mail import EmailMultiAlternatives
 from django.template import Context
-from django.template.loader import get_template, render_to_string
+from django.template.loader import render_to_string
 
 from codalab import settings
 from .models import NewsletterSubscription
@@ -25,74 +25,80 @@ def _send_mail(context_data, from_email=None, html_file=None, text_file=None, su
 
 
 def newsletter_signup(request):
-    form = NewsletterSubscriptionSignUpForm(request.POST or None)
+    if settings.MAILCHIMP_API_KEY:
+        form = NewsletterSubscriptionSignUpForm(request.POST or None)
 
-    if form.is_valid():
-        instance = form.save(commit=False)
-        if NewsletterSubscription.objects.filter(email=instance.email).exists():
-            messages.warning(request, 'This email already signed up for newsletters',
-                             'alert alert-warning alert-dismissible')
+        if form.is_valid():
+            instance = form.save(commit=False)
+            if NewsletterSubscription.objects.filter(email=instance.email).exists():
+                messages.warning(request, 'This email already signed up for newsletters',
+                                 'alert alert-warning alert-dismissible')
+            else:
+                data = {
+                    "email_address": instance.email,
+                    "status": "subscribed",
+                }
+
+                NewsletterSubscription.objects.create(email=instance.email).subscribe()
+
+                messages.success(request, 'You have been added to the Codalab newsletter',
+                                 'alert alert-success alert-dismissible')
+
+                subject = "Thank you for joining the Codalab newsletter"
+                to_email = instance.email
+                email_message = 'newsletter/signup_email.txt'
+                html_template = 'newsletter/signup_email.html'
+
+                _send_mail(data, html_file=html_template, text_file=email_message, subject=subject, to_email=to_email)
+
         else:
-            data = {
-                "email_address": instance.email,
-                "status": "subscribed",
-            }
-
-            NewsletterSubscription.objects.create(email=instance.email).subscribe()
-
-            messages.success(request, 'You have been added to the Codalab newsletter',
-                             'alert alert-success alert-dismissible')
-
-            subject = "Thank you for joining the Codalab newsletter"
-            to_email = instance.email
-            email_message = 'newsletter/signup_email.txt'
-            html_template = 'newsletter/signup_email.html'
-
-            _send_mail(data, html_file=html_template, text_file=email_message, subject=subject, to_email=to_email)
-
+            messages.warning(request, 'Please use a valid email address to subscribe to the newsletter',
+                             'alert alert-warning alert-dismissible')
+        context = {
+            'form': form,
+        }
+        template = "newsletter/signup.html"
     else:
-        messages.warning(request, 'Please use a valid email address to subscribe to the newsletter',
-                         'alert alert-warning alert-dismissible')
-
-    context = {
-        'form': form,
-    }
-
-    template = "newsletter/signup.html"
+        context = {}
+        template = "newsletter/404.html"
     return render(request, template, context)
 
 
 def newsletter_unsubscribe(request):
-    form = NewsletterSubscriptionUnsubscribeForm(request.POST or None)
+    if settings.MAILCHIMP_API_KEY:
+        form = NewsletterSubscriptionUnsubscribeForm(request.POST or None)
 
-    if form.is_valid():
-        email = form.cleaned_data['email']
-        if NewsletterSubscription.objects.filter(email=email).exists():
-            data = {
-                "status": "unsubscribed",
-            }
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            if NewsletterSubscription.objects.filter(email=email).exists():
+                data = {
+                    "status": "unsubscribed",
+                }
 
-            NewsletterSubscription.objects.get(email=email).unsubscribe()
+                NewsletterSubscription.objects.get(email=email).unsubscribe()
 
-            messages.success(request, 'You have been removed from the Codalab newsletter',
-                             'alert alert-success alert-dismissible')
-            subject = "You have been unsubscribed from the Codalab newsletter"
-            to_email = email
-            email_message = '/apps/newsletter/templates/newsletter/unsubscribe_email.txt'
-            html_template = 'newsletter/unsubscribe_email.html'
+                messages.success(request, 'You have been removed from the Codalab newsletter',
+                                 'alert alert-success alert-dismissible')
+                subject = "You have been unsubscribed from the Codalab newsletter"
+                to_email = email
+                email_message = '/apps/newsletter/templates/newsletter/unsubscribe_email.txt'
+                html_template = 'newsletter/unsubscribe_email.html'
 
-            _send_mail(data, html_file=html_template, text_file=email_message, subject=subject, to_email=to_email)
+                _send_mail(data, html_file=html_template, text_file=email_message, subject=subject, to_email=to_email)
+
+            else:
+                messages.warning(request, 'Your email was not found. We cannot remove that email from the newsletter',
+                                 'alert alert-warning alert-dismissible')
 
         else:
-            messages.warning(request, 'Your email was not found. We cannot remove that email from the newsletter',
+            messages.warning(request, 'Please use a valid email address to unsubscribe from the newsletter',
                              'alert alert-warning alert-dismissible')
-
+        context = {
+            'form': form,
+        }
+        template = "newsletter/unsubscribe.html"
     else:
-        messages.warning(request, 'Please use a valid email address to unsubscribe from the newsletter',
-                         'alert alert-warning alert-dismissible')
-    context = {
-        'form': form,
-    }
+        context = {}
+        template = "newsletter/404.html"
 
-    template = "newsletter/unsubscribe.html"
     return render(request, template, context)
