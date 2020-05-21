0e8b446220a6d441ec26ef361d965bc5ce225917
Eric Carmichael <eric@ckcollab.com>
2020-05-20 18:21:56 -0700

    add hostnames to simple health

--- codalab/apps/health/views.py
+++ codalab/apps/health/views.py
@@ -118,7 +118,7 @@ def simple_health(request):
     qs = qs.order_by('-submitted_at')
     qs = qs.select_related('phase__competition')
     qs = qs.select_related('participant__user__username')
-    qs = qs.prefetch_related('phase', 'status')
+    qs = qs.prefetch_related('phase', 'status', 'metadatas')
     return render(request, "health/simple_health.html", {
         "submissions": qs[:250],
     })
