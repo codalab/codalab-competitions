e6657f0bb91dcbc35930efe56b2fb30eb8397270
tthomas63 <mr.tyler.thomas@gmail.com>
2020-07-16 22:14:17 -0700

    Try modifying circlec_dev_setup.py to resolve memcached issues on CircleCI

--- circlec_dev_setup.py
+++ circlec_dev_setup.py
@@ -5,11 +5,22 @@ class Dev(DevBase):
     """This is needed to run circleci, it should be copied to local.py"""
     ENABLE_COMPETITIONS = True
     ENABLE_WORKSHEETS = False
+    # CACHES = {
+    #     'default': {
+    #         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    #     }
+    # }
     CACHES = {
         'default': {
             'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        },
+        'compressor': {
+            'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+            'LOCATION': 'compressor'
         }
     }
+
+    COMPRESS_CACHE_BACKEND = 'compressor'
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
