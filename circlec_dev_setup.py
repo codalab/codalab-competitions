38ee3df8c688bdbdfd19602b64788fd4b5a58317
tthomas63 <mr.tyler.thomas@gmail.com>
2020-07-17 13:01:49 -0700

    CircleCI seems to be having issues with circlec_dev_setup not overriding settings correctly. Explicitly named configuration Test and set circle.yml to use Test as DJANGO_CONFIGURATION envar

--- circlec_dev_setup.py
+++ circlec_dev_setup.py
@@ -1,26 +1,15 @@
 from base import DevBase
 
 
-class Dev(DevBase):
+class Test(DevBase):
     """This is needed to run circleci, it should be copied to local.py"""
     ENABLE_COMPETITIONS = True
     ENABLE_WORKSHEETS = False
-    # CACHES = {
-    #     'default': {
-    #         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-    #     }
-    # }
     CACHES = {
         'default': {
             'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-        },
-        'compressor': {
-            'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-            'LOCATION': 'compressor'
         }
     }
-
-    COMPRESS_CACHE_BACKEND = 'compressor'
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
