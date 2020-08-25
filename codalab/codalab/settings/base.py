e590901f9d238077c870e1bc0586cb95e170bc9f
Eric Carmichael <eric@ckcollab.com>
2020-08-24 17:33:03 -0700

    Submissions list improvements (#2799)
    * saving spot, basics of v-data-table pulling submissions working
    
    * show/hide migrate & migrated logic working
    
    * added migrate behavior to submission mgmt
    
    * pull cruft
    
    * refactor styling, match codalab v1.5 better
    
    * mark submission as failed properly

--- codalab/codalab/settings/base.py
+++ codalab/codalab/settings/base.py
@@ -291,6 +291,7 @@ class Base(Settings):
     REST_FRAMEWORK = {
         'DEFAULT_RENDERER_CLASSES': (
             'rest_framework.renderers.JSONRenderer',
+            'rest_framework.renderers.BrowsableAPIRenderer',
         ),
     }
 
