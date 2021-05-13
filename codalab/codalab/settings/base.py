3bae5fcb2952ff09a871a47d3d832c1519f323ce
tthomas63 <mr.tyler.thomas@gmail.com>
2021-05-13 10:23:48 -0700

    Correct default endpoint_url format

--- codalab/codalab/settings/base.py
+++ codalab/codalab/settings/base.py
@@ -365,7 +365,7 @@ class Base(Configuration):
     AWS_S3_CALLING_FORMAT = os.environ.get('AWS_S3_CALLING_FORMAT', 'boto.s3.connection.OrdinaryCallingFormat')
     AWS_S3_HOST = os.environ.get('AWS_S3_HOST', 's3-us-west-2.amazonaws.com')
 
-    AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL','s3.amazonaws.com')
+    AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL','https://s3.amazonaws.com')
     AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')
     AWS_S3_SIGNATURE_VERSION = os.environ.get('AWS_S3_SIGNATURE_VERSION', 's3v4')
 
