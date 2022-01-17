aa0cb7ff3163531058e4fd8bc6d6ce2c7ecfa5ec
bbearce <bbearce@gmail.com>
2022-01-17 19:34:18 +0000

    Use web fixture to automatically upload the CODALAB_STIE_DOMAIN from .env on deploy.

--- docker/run_django.sh
+++ docker/run_django.sh
@@ -21,6 +21,34 @@ python manage.py collectstatic --noinput
 
 python manage.py migrate
 
+# Use node to edit site domain from .env
+node > out_${jsonFile} <<EOF
+//Read data
+var data = require('./apps/web/fixtures/initial_data.json')  
+
+//Manipulate data
+data.forEach((v,i,a) =>{
+  if (v.model === "sites.site"){
+    console.log(`Changing domain: ${v.fields.domain} to ${process.env.CODALAB_SITE_DOMAIN}`)
+    v.fields.domain = process.env.CODALAB_SITE_DOMAIN
+    v.fields.name = 'CODALAB_SITE_DOMAIN'
+  }
+})
+
+
+//Output data
+const fs = require('fs')
+content = JSON.stringify(data)
+fs.writeFile('./apps/web/fixtures/initial_data.json', content, err => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  //file written successfully
+})
+
+EOF
+
 # For Django 1.7 we cannot run this here. Now using fixtures with loaddata, keeping this here for history
 # Insert initial data into the database
 
