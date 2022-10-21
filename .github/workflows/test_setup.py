2dde18344bca330fdbb26b1ef4227b2c163f13ad
Xavier Baró <xavierbaro@gmail.com>
2022-10-21 11:59:27 +0200

    Update test_setup.py
    Use sqlite3 as database for tests

--- .github/workflows/test_setup.py
+++ .github/workflows/test_setup.py
@@ -10,9 +10,9 @@ class Test(DevBase):
             'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
         }
     }
-    #DATABASES = {
-    #    'default': {
-    #        'ENGINE': 'django.db.backends.sqlite3',
-    #        'NAME': ':memory:',
-    #    }
-    #}
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
+    }
