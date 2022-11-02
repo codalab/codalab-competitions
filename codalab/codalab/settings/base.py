a5382ed373e49b35b296cbdcd8cbd8638737efc8
Adrien Pavão <adrien.pavao@gmail.com>
2022-11-02 16:28:26 +0100

    Update CODALAB_VERSION

--- codalab/codalab/settings/base.py
+++ codalab/codalab/settings/base.py
@@ -80,7 +80,7 @@ class Base(Configuration):
 
     AUTH_USER_MODEL = 'authenz.ClUser'
 
-    CODALAB_VERSION = '1.5'
+    CODALAB_VERSION = '1.6'
 
     # Hosts/domain names that are valid for this site; required if DEBUG is False
     # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
