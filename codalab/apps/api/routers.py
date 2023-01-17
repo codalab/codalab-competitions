c5d7d8ab2986f8406e355fb17c026916b4239e90
dinh_tuan tran <dinh_tuan.tran@lisn.fr>
2023-01-17 14:33:26 +0100

    - Adding API docs

--- codalab/apps/api/routers.py
+++ codalab/apps/api/routers.py
@@ -3,8 +3,11 @@ from apps.api.views import storage_views as storage_views
 from apps.api.views import admin_views as admin_views
 from django.conf.urls import url
 from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
+
 
 router = routers.DefaultRouter()
+
 
 router.register(r'competition/(?P<competition_id>\d+)/participants', views.CompetitionParticipantAPIViewSet)
 router.register(r'competition', views.CompetitionAPIViewSet)
@@ -19,17 +22,17 @@ urlpatterns += (
     url(r'^competition/create/sas$', views.CompetitionCreationSasApi.as_view(), name='api_competition_creation_sas'),
     url(r'^competition/create/(?P<token>\d+)$', views.CompetitionCreationStatusApi.as_view(), name='api_competition_creation_status'),
 
-    url(r'^competition/(?P<competition_id>\d+)/submission$',views.competition_submission_create,name='api_competition_submission_post'),
-    url(r'^competition/(?P<competition_id>\d+)/submission/sas$',views.CompetitionSubmissionSasApi.as_view(), name='api_competition_submission_sas'),
-    url(r'^competition/(?P<competition_id>\d+)/submission/(?P<pk>\d+)$',views.competition_submission_retrieve,name='api_competition_submission_get'),
-    url(r'^competition/(?P<competition_id>\d+)/submission/(?P<pk>\d+)/leaderboard$',views.competition_submission_leaderboard,name='api_competition_submission_leaderboard'),
+    url(r'^competition/(?P<competition_id>\d+)/submission$', views.competition_submission_create, name='api_competition_submission_post'),
+    url(r'^competition/(?P<competition_id>\d+)/submission/sas$', views.CompetitionSubmissionSasApi.as_view(), name='api_competition_submission_sas'),
+    url(r'^competition/(?P<competition_id>\d+)/submission/(?P<pk>\d+)$', views.competition_submission_retrieve, name='api_competition_submission_get'),
+    url(r'^competition/(?P<competition_id>\d+)/submission/(?P<pk>\d+)/leaderboard$', views.competition_submission_leaderboard, name='api_competition_submission_leaderboard'),
     url(r'^competition/(?P<competition_id>\d+)/submissions/?$', views.CompetitionSubmissionListViewSet.as_view({'get': 'list'}), name='api_competition_submission_list'),
 
-    url(r'^competition/(?P<pk>\d+)/phases/(?P<phasenumber>\d+)$',views.competitionphase_retrieve,name='api_competitionphase'),
-    url(r'^competition/(?P<competition_id>\d+)/phases/(?P<phase_id>\d+)/leaderboard$',views.leaderboard_retrieve, name='api_phase_leaderboard'),
-    url(r'^competition/(?P<competition_id>\d+)/phases/(?P<phase_id>\d+)/leaderboard/data$',views.LeaderBoardDataViewSet.as_view(), name='api_phase_leaderboarddata'),
+    url(r'^competition/(?P<pk>\d+)/phases/(?P<phasenumber>\d+)$', views.competitionphase_retrieve, name='api_competitionphase'),
+    url(r'^competition/(?P<competition_id>\d+)/phases/(?P<phase_id>\d+)/leaderboard$', views.leaderboard_retrieve, name='api_phase_leaderboard'),
+    url(r'^competition/(?P<competition_id>\d+)/phases/(?P<phase_id>\d+)/leaderboard/data$', views.LeaderBoardDataViewSet.as_view(), name='api_phase_leaderboarddata'),
 
-    url(r'^competition/(?P<pk>\d+)/phases/$', views.competitionphase_list,name='api_competitionphases_list'),
+    url(r'^competition/(?P<pk>\d+)/phases/$', views.competitionphase_list, name='api_competitionphases_list'),
 
     url(r'^competition/(?P<competition_id>\d+)/pages/(?P<category>[a-zA-Z][\w\d\-\_]*)/$', views.competition_page_list, name='api_competition_page_list'),
     url(r'^competition/(?P<competition_id>\d+)/pages/(?P<pk>\d+)$', views.competition_page, name='api_competition_page'),
@@ -45,5 +48,7 @@ urlpatterns += (
     url(r'^admin/competitions/list', admin_views.GetCompetitions.as_view(), name="competitions"),
     url(r'^admin/competitions/update', admin_views.UpdateCompetitions.as_view(), name="update_competitions"),
     url(r'^admin/competition/(?P<competition_id>\d+)/apply_upper_bound_limit', admin_views.ApplyUpperBoundLimit.as_view(), name="apply_upper_bound_limit"),
-    url(r'^admin/competitions/default_upper_bound_limit', admin_views.GetDefaultUpperBoundLimit.as_view(), name="get_default_upper_bound_limit")
+    url(r'^admin/competitions/default_upper_bound_limit', admin_views.GetDefaultUpperBoundLimit.as_view(), name="get_default_upper_bound_limit"),
+    # API Docs
+    url(r'^docs/', include_docs_urls(title='Codalab API Documentation', public=False))
 )
